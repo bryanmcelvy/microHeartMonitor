17c7a96f4c4b9aa31e5ae2f40b18d230962fa3a1
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-21 17:13:28 -0500

    Add UART1 functions

--- src/drivers/UART.c
+++ src/drivers/UART.c
@@ -6,6 +6,10 @@
 
 #include "UART.h"
 
+/**********************************************************************
+UART0
+***********************************************************************/
+
 void UART0_Init(void) {
     /**
      *  @details
@@ -66,6 +70,74 @@ void UART0_WriteStr(unsigned char * str_ptr) {
      */
     while (*str_ptr != '\0') {
         UART0_WriteChar(*str_ptr);
+        str_ptr += 1;
+    }
+}
+
+/**********************************************************************
+UART1
+***********************************************************************/
+
+void UART1_Init(void) {
+    /**
+     *  @details
+     *  Given the bus frequency (`f_bus`) and desired baud rate (`BR`),
+     *  the baud rate divisor (`BRD`) can be calculated:
+     *      \f$BRD = f_{bus}/(16*BR)\f$
+     * 
+     *  The integer BRD (`IBRD`) is simply the integer part of the BRD:
+     *      \f$IBRD = int( BRD )\f$
+     * 
+     *  The fractional BRD (`FBRD`) is calculated using the fractional part (`mod(BRD,1)`) of the BRD:
+     *      \f$FBRD = int( ( mod(BRD,1) * 64 ) + 0.5 )\f$
+     */
+    
+    SYSCTL_RCGCUART_R |= 0x02;                  // activate clock for UART1
+    if ((SYSCTL_RCGCGPIO_R & 0x02) == 0) {
+        SYSCTL_RCGCGPIO_R |= 0x02;              // activate clock for GPIO Port B
+    }
+
+    UART1_CTL_R &= ~(0x01);                     // disable UART1
+    UART1_IBRD_R |= 43;                         /// NOTE: LCRH must be accessed *AFTER* setting the `BRD` register
+    UART1_FBRD_R |= 26;
+    UART1_LCRH_R |= 0x70;                       // 8-bit length, FIFO
+    UART1_CC_R &= ~(0x0F);                      // system clock source
+    UART1_CTL_R |= 0x01;                        // re-enable UART0
+
+    GPIO_PORTB_AFSEL_R |= 0x03;                 // alt. mode for PB0/1
+    GPIO_PORTB_PCTL_R |= 0x11;                  // UART mode for PB0/1
+    GPIO_PORTB_DR8R_R |= 0x03;                  // 2[ma] drive strength
+    GPIO_PORTB_AMSEL_R &= ~(0x03);              // disable analog
+    GPIO_PORTB_DEN_R |= 0x03;                   // enable digital I/O
+}
+
+unsigned char UART1_ReadChar(void) {
+    /**
+     * This function uses busy-wait synchronization 
+     * to read a character from UART1.
+     */
+    while( (UART1_FR_R & 0x10) != 0) {}         // wait until Rx FIFO is empty
+    return (unsigned char) (UART1_DR_R & 0xFF);
+}
+
+void UART1_WriteChar(unsigned char input_char) {
+    /**
+     * This function uses busy-wait synchronization 
+     * to write a character to UART1.
+     */
+    while( (UART1_FR_R & 0x20) != 0) {}         // wait until Tx FIFO is no longer full
+    UART1_DR_R = input_char;
+}
+
+void UART1_WriteStr(unsigned char * str_ptr) {
+    /**
+     * This function uses UART1_WriteChar() function 
+     * to write a C string to UART0. The function writes 
+     * until either the entire string has been written or 
+     * a null-terminated character has been reached.
+     */
+    while (*str_ptr != '\0') {
+        UART1_WriteChar(*str_ptr);
         str_ptr += 1;
     }
 }
--- src/drivers/UART.h
+++ src/drivers/UART.h
@@ -1,7 +1,13 @@
 /**
  * @file        UART.h
  * @author      Bryan McElvy
- * @brief       Driver module for UART1.
+ * @brief       Driver module for serial communication via UART0 and UART 1.
+ * 
+ *              UART0 uses PA0 and PA1, which are not broken out but do connect
+ *              to a PC's serial port via USB.
+ * 
+ *              UART1 uses PB0 (Rx) and PB1 (Tx), which are not broken out but
+ *              do not connect to a serial port.
  * 
  * @addtogroup  uart
  * @{
@@ -11,6 +17,10 @@
 #define __UART_H___
 
 #include "tm4c123gh6pm.h"
+
+/**********************************************************************
+UART0
+***********************************************************************/
 
 /**
  * @brief   Initialize UART0 to a baud rate of 115200, 
@@ -38,6 +48,38 @@ void UART0_WriteChar(unsigned char input_char);
  * @param   str_ptr pointer to C string
  */
 void UART0_WriteStr(unsigned char * str_ptr);
+
+/**********************************************************************
+UART1
+***********************************************************************/
+
+/**
+ * @brief   Initialize UART1 to a baud rate of 115200, 
+ *          8-bit data length, 1 start bit, and 1 stop bit.
+ */
+void UART1_Init(void);
+
+/**
+ * @brief   Read a single character from UART1.
+ * 
+ * @return  input_char
+ */
+unsigned char UART1_ReadChar(void);
+
+/**
+ * @brief   Write a single character to UART1.
+ * 
+ * @param   input_char
+ */
+void UART1_WriteChar(unsigned char input_char);
+
+/**
+ * @brief   Write a C string to UART1.
+ * 
+ * @param   str_ptr pointer to C string
+ */
+void UART1_WriteStr(unsigned char * str_ptr);
+
 
 #endif
 
