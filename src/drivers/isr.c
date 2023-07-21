d06cd0966cd1289f409e7cc0c3b7edc6ae998ae9
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-20 22:42:44 -0500

    Fix group command, add refs to init. funcs

--- src/drivers/isr.c
+++ src/drivers/isr.c
@@ -1,27 +1,30 @@
 /**
+ * @addtogroup  thread
+ * @{
+ * 
  * @file    isr.c
  * @author  Bryan McElvy
  * @brief   Source code for interrupt service routines (ISRs)
- * 
- * @addtogroup  thread
- * @{
  */
 
 /**
  * @brief   ISR for facilitating user control of program state.
  * 
+ * Initialized by GPIO_PF_Interrupt_Init() from the @ref gpio "GPIO" module.
  */
 void GPIO_PortF_Handler() {}
 
 /**
- * @brief   ISR for collecting ECG samples @ f_s = 200 [Hz]
+ * @brief   ISR for collecting ECG samples @ \f$f_s = 200 [Hz]\f$.
  * 
+ * Initialized by SysTick_Interrupt_Init() from the @ref systick "SysTick" module.
  */
 void SysTick_Handler() {}
 
 /**
- * @brief   ISR for updating the LCD @ f_s = 30 [Hz]
+ * @brief   ISR for updating the LCD @ \f$f_s = 30 [Hz]\f$.
  * 
+ * Initialized by Timer1A_Init() from the @ref timer "Timer" module.
  */
 void Timer1A_Handler() {}
 
