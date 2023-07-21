fa3d9128de3702e35d937d606836c82018b27814
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-20 19:45:52 -0500

    Add Timer2A funcs

--- src/drivers/Timer.c
+++ src/drivers/Timer.c
@@ -6,6 +6,9 @@
 
 #include "Timer.h"
 
+/**********************************************************************
+Timer0A
+***********************************************************************/
 void Timer0A_Init(void) {
     SYSCTL_RCGCTIMER_R |= 0x01;             // enable clock for timer 0
     TIMER0_CTL_R &= ~(0x101);               // disable both subtimers
@@ -34,6 +37,10 @@ void Timer0A_Wait1ms(uint32_t time_ms) {
     while (Timer0A_isCounting()) {}
 }
 
+/**********************************************************************
+Timer0A
+***********************************************************************/
+
 void Timer1A_Init(uint32_t time_ms) {
     uint32_t reload_val;
     
@@ -51,4 +58,35 @@ void Timer1A_Init(uint32_t time_ms) {
     NVIC_EN0_R |= 0x200000;                 // enable Timer 1A interrupts in NVIC
 
     TIMER1_CTL_R |= 0x101;                  // enable both subtimers
+}
+
+/**********************************************************************
+Timer2A
+***********************************************************************/
+void Timer2A_Init(void) {
+    SYSCTL_RCGCTIMER_R |= 0x04;             // enable clock for timer 2
+    TIMER2_CTL_R &= ~(0x101);               // disable both subtimers
+    TIMER2_CFG_R &= ~(0x07);                // 32-bit timer configuration
+    TIMER2_TAMR_R |= 0x01;                  // one-shot mode, countdown
+}
+
+void Timer2A_Start(uint32_t time_ms) {
+    uint32_t reload_val;
+    
+    TIMER2_CTL_R &= ~(0x101);               // disable Timer 2
+
+    time_ms = (time_ms <= 53000) ? ((uint32_t) time_ms) : 53000;
+    reload_val = (80000 * time_ms) - 1;
+    TIMER2_TAILR_R = reload_val;
+
+    TIMER2_CTL_R |= 0x101;                  // re-enable Timer 2
+}
+
+uint8_t Timer2A_isCounting(void) {
+    return TIMER2_CTL_R & 0x101;             // check enable bit (1 if counting, 0 if not)
+}
+
+void Time20A_Wait1ms(uint32_t time_ms) {
+    Timer2A_Start(time_ms);
+    while (Timer2A_isCounting()) {}
 }
--- src/drivers/Timer.h
+++ src/drivers/Timer.h
@@ -9,6 +9,10 @@
 
 #include "tm4c123gh6pm.h"
 
+/**********************************************************************
+Timer0A
+***********************************************************************/
+
 /**
  * @brief   Initialize timer 0 as 32-bit, one-shot, countdown timer.
  * 
@@ -38,6 +42,10 @@ uint8_t Timer0A_isCounting(void);
  */
 void Timer0A_Wait1ms(uint32_t time_ms);
 
+/**********************************************************************
+Timer1A
+***********************************************************************/
+
 /**
  * @brief   Initialize timer 1 as a 32-bit, periodic, countdown timer with interrupts.
  * 
@@ -45,5 +53,39 @@ void Timer0A_Wait1ms(uint32_t time_ms);
  *                  Must be <= 53 seconds.
  */
 void Timer1A_Init(uint32_t time_ms);
+
+/**********************************************************************
+Timer2A
+***********************************************************************/
+
+/**
+ * @brief   Initialize timer 2 as 32-bit, one-shot, countdown timer.
+ * 
+ */
+void Timer2A_Init(void);
+
+/**
+ * @brief   Count down starting from the inputted value.
+ * 
+ * @param   time_ms Time in [ms] to load into Timer 2. 
+ *                  Must be <= 53 seconds.
+ */
+void Timer2A_Start(uint32_t time_ms);
+
+/**
+ * @brief   Returns 1 if Timer2 is still counting and 0 if not.
+ * 
+ * @return  uint8_t status
+ */
+uint8_t Timer2A_isCounting(void);
+
+/**
+ * @brief   Wait for the specified amount of time in [ms].
+ * 
+ * @param   time_ms Time in [ms] to load into Timer 2. 
+ *                  Must be <= 53 seconds.
+ */
+void Timer2A_Wait1ms(uint32_t time_ms);
+
 
 #endif
