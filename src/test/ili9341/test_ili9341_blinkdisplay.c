1ce35234dab59eaea77e23e6f0baf2c0da41378b
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-22 18:24:02 -0500

    Complete superloop

--- src/test/ili9341/test_ili9341_blinkdisplay.c
+++ src/test/ili9341/test_ili9341_blinkdisplay.c
@@ -3,7 +3,13 @@
 
 int main() {
     PLL_Init();
+    Timer0A_Init();     // Timer2A is reserved for the ILI9341 module
     ILI9341_Init();
 
-    
+    while(1) {
+        ILI9341_DisplayOn(1);
+        Timer0A_Wait1ms(500);
+        ILI9341_DisplayOn(0);
+        Timer0A_Wait1ms(500);
+    }
 }
