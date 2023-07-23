bbcc1a32d8a8d22ff0d9524f9c2e0714ba3eb11a
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-22 22:22:47 -0500

    Add LCD module

---
+++ src/app/LCD.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Module for outputting the ECG waveform and HR to a liquid crystal display (LCD).
+ */
+
+#ifndef __LCD_H__
+#define __LCD_H__
+
+#include "ILI9341.h"
+
+void LCD_Init(void);
+
+#endif
