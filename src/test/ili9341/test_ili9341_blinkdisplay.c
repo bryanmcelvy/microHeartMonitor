56604bfa5e936309e5ad8cde647adab716fe5daf
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-28 23:30:39 -0500

    Revamp blinkdisplay test script

--- src/test/ili9341/test_ili9341_blinkdisplay.c
+++ src/test/ili9341/test_ili9341_blinkdisplay.c
@@ -1,15 +1,60 @@
 #include "PLL.h"
 #include "ILI9341.h"
+#include "Timer.h"
+#include "GPIO.h"
 
-int main() {
+enum colors {   RED =   (uint8_t)   0x02, 
+                GREEN = (uint8_t)   0x08, 
+                BLUE =  (uint8_t)   0x04
+};
+
+int main(void) {
+    unsigned char color;
+    uint8_t red_val, green_val, blue_val;
+    
     PLL_Init();
-    Timer0A_Init();     // Timer2A is reserved for the ILI9341 module
     ILI9341_Init();
+    Timer0A_Init();     // Timer2A is reserved for the ILI9341 module
+    GPIO_PF_LED_Init();
+    ILI9341_setRowAddress(0, NUM_ROWS-1);
+    ILI9341_setColAddress(0, NUM_COLS-1);
+    // ILI9341_setDisplayStatus(1);
 
+    color = BLUE;
     while(1) {
-        ILI9341_DisplayOn(1);
-        Timer0A_Wait1ms(500);
-        ILI9341_DisplayOn(0);
-        Timer0A_Wait1ms(500);
+        // Determine next color
+        switch(color) {
+            case RED: 
+                red_val = 0;
+                green_val = 255;
+                blue_val = 0;
+                color = GREEN;
+                break;
+            case GREEN: 
+                red_val = 0;
+                green_val = 0;
+                blue_val = 255;
+                color = BLUE;
+                break;
+            case BLUE:
+                red_val = 255;
+                green_val = 0;
+                blue_val = 0;
+                color = RED;
+                break;
+        }
+        while(Timer0A_isCounting());
+        
+        // Write to display
+        Timer0A_Start(1000);
+
+        GPIO_PF_LED_Write(0x0F, 0);
+        GPIO_PF_LED_Write(color, 1);
+        ILI9341_writeMemCmd();
+        for (uint32_t i = 0; i < 76800; i++) {
+            ILI9341_write1px(blue_val, green_val, red_val);
+        }
+        // ILI9341_NoOpCmd();
+
     }
 }
