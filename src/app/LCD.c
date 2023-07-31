0089b9b6abd9c83b397e545ea9a5ad2257e84487
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-31 00:16:32 -0500

    Define and implement several functions

--- src/app/LCD.c
+++ src/app/LCD.c
@@ -4,6 +4,9 @@
  * @brief   Source code for LCD module.
  */
 
+/******************************************************************************
+Includes
+*******************************************************************************/
 #include "LCD.h"
 
 #include "ILI9341.h"
@@ -15,6 +18,196 @@
 
 #include <stdint.h>
 
+/******************************************************************************
+Declarations
+*******************************************************************************/
+
+// Function Prototypes
+static void LCD_updateNumPixels(void);
+static void LCD_drawLine(uint16_t center, uint16_t lineWidth, bool is_row);
+
+// LCD Object
+struct LCD {
+    uint16_t rowStart;
+    uint16_t rowEnd;
+    uint16_t colStart;
+    uint16_t colEnd;
+    uint32_t numPixels;
+
+    uint8_t R_val;
+    uint8_t G_val;
+    uint8_t B_val;
+
+    bool is_ON;
+    bool is_inverted;
+
+    bool is_16bit;
+    bool is_init;
+};
+
+static struct LCD lcd = {  
+    0, (NUM_ROWS-1),                // entire area
+    0, (NUM_COLS-1),                // colStart, colEnd
+    (NUM_ROWS * NUM_COLS),          // numPixels
+
+    255, 0, 0,                      // write color is red
+
+    true,                           // display is on
+    false,                          // not inverted
+    true,                           // 16-bit color depth
+    false                           // not initialized
+};
+
+/******************************************************************************
+Initialization and Configuration
+*******************************************************************************/
+
 void LCD_Init(void) {
+    if (lcd.is_init == false) {
+        ILI9341_Init();
+        ILI9341_setMemAccessCtrl(0, 0, 0, 0, 1, 0);
+        lcd.is_init = true;
+    }
+}
+
+void LCD_toggleStatus(void) {
+    lcd.is_ON = !(lcd.is_ON);
+    ILI9341_setDisplayStatus(lcd.is_ON);
+}
+
+void LCD_toggleInversion(void) {
+    lcd.is_inverted = !(lcd.is_inverted);
+    ILI9341_setDispInversion(lcd.is_inverted);
+}
+
+void LCD_toggleColorDepth(void) {
+    lcd.is_16bit = !(lcd.is_16bit);
+    ILI9341_setColorDepth(lcd.is_16bit);
+    if (lcd.is_16bit) {                     // convert R and B to 5-bits
+        lcd.R_val |= 0x1F;
+        lcd.B_val |= 0x1F;
+    }
+}
+
+/******************************************************************************
+Drawing Area
+*******************************************************************************/
+
+static void LCD_updateNumPixels(void) {
+    lcd.numPixels = ((lcd.rowEnd - lcd.rowStart) + 1) 
+                    * ((lcd.colEnd - lcd.colStart) + 1);
+}
+
+void LCD_setArea(   uint16_t rowStartNew, uint16_t rowEndNew,
+                    uint16_t colStartNew, uint16_t colEndNew) {
+    lcd.rowEnd = (rowEndNew < NUM_ROWS) ? rowEndNew : (NUM_ROWS - 1);
+    lcd.rowStart = (rowStartNew < rowEndNew) ? rowStartNew : (rowEndNew);
+
+    lcd.colEnd = (colEndNew < NUM_COLS) ? colEndNew : (NUM_COLS - 1);
+    lcd.colStart = (colStartNew < colEndNew) ? colStartNew : (colEndNew);
+
+    LCD_updateNumPixels();
+}
+
+void LCD_setRow(uint16_t rowStartNew, uint16_t rowEndNew) {
+    lcd.rowEnd = (rowEndNew < NUM_ROWS) ? rowEndNew : (NUM_ROWS - 1);
+    lcd.rowStart = (rowStartNew < rowEndNew) ? rowStartNew : (rowEndNew);
+    LCD_updateNumPixels();
+}
+
+void LCD_setCol(uint16_t colStartNew, uint16_t colEndNew) {
+    lcd.colEnd = (colEndNew < NUM_COLS) ? colEndNew : (NUM_COLS - 1);
+    lcd.colStart = (colStartNew < colEndNew) ? colStartNew : (colEndNew);
+    LCD_updateNumPixels();
+}
+
+/******************************************************************************
+Color
+*******************************************************************************/
+
+void LCD_setColor(uint8_t R_val, uint8_t G_val, uint8_t B_val) {
+    lcd.R_val = (lcd.is_16bit) ? (R_val & 0x1F) : (R_val & 0x3F);
+    lcd.G_val = G_val & 0x3F;
+    lcd.B_val = (lcd.is_16bit) ? (B_val & 0x1F) : (B_val & 0x3F);
+}
+
+void LCD_setColor_3bit(uint8_t color_code) {
+    /**
+        This is simply a convenience function for `LCD_setColor()`.
+        The following table shows what the output color will be:
+
+        hex | binary | pixel color
+        -------------|------------
+        0x04|  100   |    red 
+        0x06|  110   |    yellow 
+        0x02|  010   |    green 
+        0x03|  011   |    cyan 
+        0x01|  001   |    blue 
+        0x05|  101   |    purple 
+        0x07|  111   |    white 
+     */
+    
+    // use white if `color_code` == 0
+    color_code = (color_code > 0) ? (color_code & 0x07) : 0x07;
+
+    LCD_setColor(   255 * (color_code & 0x04), 
+                    255 * (color_code & 0x02),
+                    255 * (color_code & 0x01)   );
+}
+
+/******************************************************************************
+Drawing
+*******************************************************************************/
+
+void LCD_draw(void) {
+    ILI9341_setRowAddress(lcd.rowStart, lcd.rowEnd);
+    ILI9341_setColAddress(lcd.colStart, lcd.colEnd);
+
+    ILI9341_writeMemCmd();
+    for (int count = 0; count < lcd.numPixels; count ++) {
+        ILI9341_write1px(lcd.R_val, lcd.G_val, lcd.B_val, lcd.is_16bit);
+    }
+}
+
+static void LCD_drawLine(uint16_t center, uint16_t lineWidth, bool is_row) {
+    uint16_t start, end;
+    uint16_t padding;
+    uint16_t max_num;
+
+    max_num = (is_row) ? NUM_ROWS : NUM_COLS;
+
+    lineWidth = ((lineWidth % 2) == 0) ? lineWidth : (lineWidth - 1);
+    padding = ( (lineWidth - 1) / 2 );
+
+    if (center < padding) {
+        center = padding + 1;
+    }
+    else if ( center >= (max_num - padding) ) {
+        center = (max_num - padding) - 1;
+    }
+
+    start = center - padding;
+    end = center + padding;
+
+    if (is_row) {
+        LCD_setArea(start, end, 0, (NUM_COLS-1));
+    }
+    else {
+        LCD_setArea(0, (NUM_ROWS-1), start, end);
+    }
+    LCD_draw();
+}
+
+void LCD_drawHLine(uint16_t rowCenter, uint16_t lineWidth) {
+    LCD_drawLine(rowCenter, lineWidth, 1);
+}
+
+void LCD_drawVLine(uint16_t colCenter, uint16_t lineWidth) {
+    LCD_drawLine(colCenter, lineWidth, 0);
+}
+
+//TODO: Write
+void LCD_drawSquare(uint16_t startRow, uint16_t startCol, uint16_t N) {
 
 }
+
--- src/app/LCD.h
+++ src/app/LCD.h
@@ -7,6 +7,9 @@
 #ifndef __LCD_H__
 #define __LCD_H__
 
+/******************************************************************************
+Includes
+*******************************************************************************/
 #include "ILI9341.h"
 
 #include "SPI.h"
@@ -16,10 +19,117 @@
 
 #include <stdint.h>
 
+/******************************************************************************
+Initialization and Configuration
+*******************************************************************************/
+void LCD_Init(void);
+
 /**
- * @brief   Initializes and configures the ILI9341 LCD driver.
+ * @brief   Toggle the display ON or OFF (ON by default)
+ */
+void LCD_toggleStatus(void);
+
+/**
+ * @brief   Toggle display inversion ON or OFF (OFF by default)
  * 
  */
-void LCD_Init(void);
+void LCD_toggleInversion(void);
+
+/**
+ * @brief   Toggle 16-bit or 18-bit color depth (16-bit by default)
+ * 
+ */
+void LCD_toggleColorDepth(void);
+
+/******************************************************************************
+Drawing Area
+*******************************************************************************/
+
+/**
+ * @brief           Set the area of the display to be written to.
+ * 
+ * @param rowStart  index of top-most row; `0 <= rowStart <= rowEnd`
+ * @param rowEnd    index of bottom-most row; `rowStart <= rowEnd < NUM_ROWS`
+ * @param colStart  index of left-most column; `0 <= colStart <= colEnd`
+ * @param colEnd    index of right-most column; `colStart <= colEnd < NUM_COLS`
+ */
+void LCD_setArea(   uint16_t rowStart, uint16_t rowEnd,
+                    uint16_t colStart, uint16_t colEnd);
+
+/**
+ * @brief           Set only the rows to be written to.
+ * 
+ * @param rowStart  index of top-most row; `0 <= rowStart <= rowEnd`
+ * @param rowEnd    index of bottom-most row; `rowStart <= rowEnd < NUM_ROWS`
+ */
+void LCD_setRow(uint16_t rowStart, uint16_t rowEnd);
+
+/**
+ * @brief           Set only the columns to be written to.
+ * 
+ * @param colStart  index of left-most column; `0 <= colStart <= colEnd`
+ * @param colEnd    index of right-most column; `colStart <= colEnd < NUM_COLS`
+ */
+void LCD_setCol(uint16_t colStart, uint16_t colEnd);
+
+/******************************************************************************
+Color
+*******************************************************************************/
+
+/**
+ * @brief           Set the current color value for the display.
+ *                  Only the first 5-6 bits are used.
+ * 
+ * @param   R_val   5-bit (0-31) R value; 6-bit (0-63) if color depth is 18-bit
+ * @param   G_val   6-bit (0-63) G value
+ * @param   B_val   5-bit (0-31) B value; 6-bit (0-63) if color depth is 18-bit
+ */
+void LCD_setColor(uint8_t R_val, uint8_t G_val, uint8_t B_val);
+
+/**
+ * @brief               Set the color value via a 3-bit code.
+ * 
+ * @param color_code    3-bit color value to use. Bits 2, 1, 0 correspond to
+ *                      R, G, and B values, respectively.
+ */
+void LCD_setColor_3bit(uint8_t color_code);
+
+/******************************************************************************
+Drawing
+*******************************************************************************/
+
+/**
+ * @brief   Draw on the LCD display. This function should be called after
+ *          setting the drawable area via `LCD_setArea()`, or after 
+ *          individually setting the row and column areas using `LCD_setRow`
+ *          and `LCD_setCol`, respectively.
+ */
+void LCD_draw(void);
+
+/**
+ * @brief   Draw a horizontal line onto the display.
+ * 
+ * @param   rowCenter row that the line will be centered on
+ * @param   lineWidth width of the line; needs to be odd
+ */
+void LCD_drawHLine(uint16_t rowCenter, uint16_t lineWidth);
+
+/**
+ * @brief   Draw a vertical line onto the display.
+ * 
+ * @param   colCenter column that the line will be centered on
+ * @param   lineWidth width of the line; needs to be odd
+ */
+void LCD_drawVLine(uint16_t colCenter, uint16_t lineWidth);
+
+/**
+ * @brief   Draw an `N` x `N` square onto the display.
+ *          The top-left corner will be located at (`startRow`, `start`Col`).
+ * 
+ * @param   startRow    index of top-most row; `0 <= startRow < NUM_ROWS`
+ * @param   startCol    index of left-most column; `0 <= startRow < NUM_COLS`
+ * @param   N           length of each side; `0 < N < NUM_COLS`
+ */
+void LCD_drawSquare(uint16_t startRow, uint16_t startCol, uint16_t N);
 
 #endif
