408788d557749599aff939df7f13d984187843dd
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-07-31 21:16:20 -0500

    Add LCD Color Test

---
+++ src/test/ili9341/test_lcd_color.c
@@ -0,0 +1,48 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Test script for writing different colors on the LCD>
+ */
+
+#include "LCD.h"
+
+#include "PLL.h"
+#include "GPIO.h"
+#include "Timer.h"
+#include <stdint.h>
+
+// const uint8_t color_display_arr[7] = {LCD_RED, LCD_YELLOW, LCD_GREEN, LCD_CYAN, LCD_BLUE, LCD_PURPLE, LCD_WHITE};
+
+int main(void) {
+    
+    // uint8_t colorOutput[3] = {0, 0, 0};
+    // uint8_t color_idx1 = 0;
+    // uint8_t color_idx2 = 2;
+
+    PLL_Init();
+    Timer0A_Init();
+    GPIO_PF_LED_Init();
+    LCD_Init();
+    LCD_toggleStatus();                         // display OFF
+
+    LCD_setColor_3bit(LCD_WHITE);
+    LCD_draw();
+
+    LCD_setColor_3bit(LCD_RED);
+    LCD_drawRectangle(0, 0, 40, 40, true);      // top left corner
+
+    LCD_setColor_3bit(LCD_GREEN);
+    LCD_drawRectangle(0, 199, 40, 40, true);    // top right corner
+
+    LCD_setColor_3bit(LCD_BLUE);
+    LCD_drawRectangle(279, 0, 40, 40, true);    // bottom left corner
+
+    LCD_setColor_3bit(LCD_PURPLE);
+    LCD_drawRectangle(279, 199, 40, 40, true);      // bottom right corner
+
+    LCD_toggleStatus();                         // display ON
+    while(1) {
+        GPIO_PF_LED_Toggle(0x08);
+        Timer0A_Wait1ms(500);
+    }
+}
