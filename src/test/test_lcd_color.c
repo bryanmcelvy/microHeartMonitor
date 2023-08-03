9eda35fe47815bcf2326a206e037dbae50e23589
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-02 21:49:19 -0500

    Refactor to test scrolling

--- src/test/test_lcd_color.c
+++ src/test/test_lcd_color.c
@@ -12,29 +12,38 @@
 #include "Timer.h"
 #include <stdint.h>
 
-// const uint8_t color_display_arr[7] = {LCD_RED, LCD_YELLOW, LCD_GREEN, LCD_CYAN, LCD_BLUE, LCD_PURPLE, LCD_WHITE};
+#define OFFSET (uint16_t) 24
+
+enum colors {   red=LCD_RED, yellow=LCD_YELLOW, green=LCD_GREEN, cyan=LCD_CYAN, 
+                blue=LCD_BLUE, purple=LCD_PURPLE, white=LCD_WHITE};
 
 int main(void) {
-    
+    // uint16_t color;
+
     PLL_Init();
     Timer0A_Init();
     GPIO_PF_LED_Init();
 
-    /*
-    TFA = VSA = BFA = 80
-    setArea(x1 = x2 = 0, y1 = 80, y2 = 160)
-
-    */
-
     LCD_Init();
 
-    LCD_setColor_3bit(LCD_RED);
-    LCD_setArea(0, NUM_ROWS, 0, 79); LCD_draw();
-    LCD_setArea(0, NUM_ROWS-1, 160, NUM_COLS-1); LCD_draw();
+    ILI9341_setScrollArea(OFFSET, X_MAX-OFFSET, 0);
+
+    LCD_setColor_3bit(red); LCD_drawRectangle(0, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(yellow); LCD_drawRectangle(40, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(green); LCD_drawRectangle(80, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(cyan); LCD_drawRectangle(120, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(blue); LCD_drawRectangle(160, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(purple); LCD_drawRectangle(200, 0, 40, Y_MAX, 1);
+    LCD_setColor_3bit(white); LCD_drawRectangle(240, 0, 40, Y_MAX, 1);
+
+    // ILI9341_setScrollStart(240);
 
     LCD_toggleStatus();                         // display ON
+    GPIO_PF_LED_Toggle(0x08);
+    int i = 0;
     while(1) {
-        GPIO_PF_LED_Toggle(0x08);
-        Timer0A_Wait1ms(500);
+        ILI9341_setScrollStart(i);
+        i = ( i < (X_MAX - 1) ) ? (i + 1) : 0;
+        Timer0A_Wait1ms(10);
     }
 }
