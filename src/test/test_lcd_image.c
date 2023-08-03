5bf1758ca3d1ee4fd7fc8c7e7359ad1de595ef95
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-03 15:12:06 -0500

    Add test script for drawing images on LCD

---
+++ src/test/test_lcd_image.c
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Test script for writing images onto the display.
+ */
+
+#include "LCD.h"
+#include "GPIO.h"
+#include "PLL.h"
+#include "Timer.h"
+
+#include "tm4c123gh6pm.h"
+
+#include "stdint.h"
+#include "stdbool.h"
+
+int main(void) {
+    PLL_Init();
+    Timer0A_Init();
+    GPIO_PF_LED_Init();
+    LCD_Init();
+
+    GPIO_PF_LED_Write(0x04, 1);
+    while(1) {
+        
+    }
+}
