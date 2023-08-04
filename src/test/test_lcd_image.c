2aa6457473443c9dfceeeb7ff1ce136db9851ebc
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-03 23:15:38 -0500

    (Near) complete test script

--- src/test/test_lcd_image.c
+++ src/test/test_lcd_image.c
@@ -17,13 +17,14 @@
 #include <stdbool.h>
 
 #define X_OFFSET        (uint16_t) 0
-#define SIZE            (uint16_t) 16
+#define SIZE            (uint16_t) 4
 
 const uint8_t COLOR_ARR[6] = {LCD_RED, LCD_YELLOW, LCD_GREEN, LCD_CYAN, LCD_BLUE, LCD_PURPLE};
 uint8_t color_idx;
 
 int main(void) {
     uint16_t x, y;
+    bool is_increasing;
     
     PLL_Init();
     Timer0A_Init();
@@ -31,6 +32,7 @@ int main(void) {
     LCD_Init();
 
     x = 0; y = 0;
+    is_increasing = true;
     color_idx = 0;
     LCD_setColor_3bit( COLOR_ARR[color_idx] );
     LCD_toggleOutput();
@@ -42,8 +44,12 @@ int main(void) {
         LCD_drawRectangle(x, y, SIZE, SIZE, true);
         Timer0A_Start(10);
 
-        x = ( x < (Y_MAX - SIZE - 1) ) ? (x + SIZE) : 0;
-        y = ( y < (Y_MAX - SIZE - 1) ) ? (y + SIZE) : 0;
+        x = ( (x + SIZE) < X_MAX ) ? (x + SIZE) : 0;
+
+        if( (y + SIZE) >= Y_MAX ) { is_increasing = false; }
+        else if(y == 0) { is_increasing = true; }
+
+        y = (is_increasing) ? (y + SIZE) : (y - SIZE);
 
         if (x == 0) {
             color_idx = (color_idx < 5) ? (color_idx + 1) : 0;
