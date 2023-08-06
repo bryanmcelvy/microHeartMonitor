46beb1eb63c5ec70ee79efeeb54ad3ed426d3387
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-05 22:34:56 -0500

    Refactor and complete Init/Get/Put funcs

--- src/common/FIFO.c
+++ src/common/FIFO.c
@@ -12,38 +12,42 @@
 #include <stdint.h>
 
 struct FIFO_t {
-    uint16_t * const start_ptr;
-    const uint16_t N;
+    uint16_t * start_ptr;
+    uint16_t N;
     uint16_t read_idx;
     uint16_t write_idx;
 };
 
-FIFO_t FIFO_Init(uint16_t N) {
-    uint16_t buffer[N];
-    FIFO_t new_fifo = {buffer, N, 0, 0};
-    return new_fifo;
+FIFO_t * FIFO_Init(FIFO_t * fifo_ptr, uint16_t buffer[], uint16_t N) {
+    fifo_ptr->start_ptr = buffer;
+    fifo_ptr->N = N;
+    fifo_ptr->read_idx = 0;
+    fifo_ptr->write_idx = 0;
+
+    return fifo_ptr;
 }
 
 uint16_t FIFO_Get(FIFO_t * fifo_ptr) {
-    FIFO_t fifo = *fifo_ptr;
     uint16_t ret_val;
 
-    if(fifo.read_idx == fifo.write_idx) {       // FIFO is empty
-        ret_val = 0;
+    if(fifo_ptr->read_idx != fifo_ptr->write_idx) {
+        ret_val = fifo_ptr->start_ptr[fifo_ptr->read_idx];
+        fifo_ptr->read_idx = (fifo_ptr->read_idx + 1) % fifo_ptr->N;            // wrap around to end
     }
-    else {
-        ret_val = fifo.start_ptr[fifo.read_idx];
-        fifo.read_idx = (fifo.read_idx > 0) ? (fifo.read_idx - 1) : (fifo.N - 1);
+    else {                                                                      // FIFO is empty
+        ret_val = 0;
     }
 
     return ret_val;
 }
 
-void FIFO_Add(FIFO_t * fifo_ptr, uint16_t val) {
-    FIFO_t fifo = *fifo_ptr;
+void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val) {
 
-    fifo.start_ptr[fifo.write_idx++] = val;
-    fifo.write_idx = (fifo.write_idx < fifo.N) ? fifo.write_idx : 0;
+    if( (fifo_ptr->write_idx + 1) % 10 != fifo_ptr->read_idx ) {                // ensure FIFO isn't full
+        fifo_ptr->start_ptr[fifo_ptr->write_idx] = val;
+        fifo_ptr->write_idx = (fifo_ptr->write_idx + 1) % fifo_ptr->N;
+    }
+
 }
 
 /** @} */
--- src/common/FIFO.h
+++ src/common/FIFO.h
@@ -17,15 +17,17 @@ typedef struct FIFO_t FIFO_t;
 /**
  * @brief           Initialize a FIFO buffer of length `N`.
  * 
- * @param N         number of elements in FIFO buffer.
- * @return          FIFO_t 
+ * @param fifo_ptr  pointer to FIFO object
+ * @param buffer    array to be used as FIFO buffer
+ * @param N         = (length of buffer) - 1; Should be <= length of buffer
+ * @return FIFO_t* 
  */
-FIFO_t FIFO_Init(uint16_t N);
+FIFO_t * FIFO_Init(FIFO_t * fifo_ptr, uint16_t buffer[], uint16_t N);
 
 /**
  * @brief           Remove the first value of the buffer.
  * 
- * @param fifo_ptr  pointer to the buffer
+ * @param fifo_ptr  pointer to FIFO object
  * @return          uint16_t 
  */
 uint16_t FIFO_Get(FIFO_t * fifo_ptr);
@@ -33,10 +35,10 @@ uint16_t FIFO_Get(FIFO_t * fifo_ptr);
 /**
  * @brief           Add a value to the end of the buffer.
  * 
- * @param fifo_ptr  pointer to the buffer
+ * @param fifo_ptr  pointer to FIFO object
  * @param val       last value in the buffer
  */
-void FIFO_Add(FIFO_t * fifo_ptr, uint16_t val);
+void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val);
 
 #endif // __FIFO_H__
 
