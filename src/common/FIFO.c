ad6e0260d1095fafafdf19f79507b8797133cb80
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-05 23:00:16 -0500

    Add FIFO_Flush(); rework docs/comments

--- src/common/FIFO.c
+++ src/common/FIFO.c
@@ -11,43 +11,50 @@
 
 #include <stdint.h>
 
-struct FIFO_t {
-    uint16_t * start_ptr;
-    uint16_t N;
-    uint16_t read_idx;
-    uint16_t write_idx;
+struct FIFO_t { 
+    uint16_t * buffer;      ///< (pointer to) array to use as FIFO buffer
+    uint16_t N;             ///< length of `buffer`
+    uint16_t front_idx;     ///< idx of front of FIFO
+    uint16_t back_idx;      ///< idx of back of FIFO
 };
 
 FIFO_t * FIFO_Init(FIFO_t * fifo_ptr, uint16_t buffer[], uint16_t N) {
-    fifo_ptr->start_ptr = buffer;
+    fifo_ptr->buffer = buffer;
     fifo_ptr->N = N;
-    fifo_ptr->read_idx = 0;
-    fifo_ptr->write_idx = 0;
+    fifo_ptr->front_idx = 0;
+    fifo_ptr->back_idx = 0;
 
     return fifo_ptr;
+}
+
+void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val) {
+    if( (fifo_ptr->back_idx + 1) % 10 != fifo_ptr->front_idx ) {                // ensure FIFO isn't full
+        fifo_ptr->buffer[fifo_ptr->back_idx] = val;
+        fifo_ptr->back_idx = (fifo_ptr->back_idx + 1) % fifo_ptr->N;          // modulo causes wrap around to 0
+    }
 }
 
 uint16_t FIFO_Get(FIFO_t * fifo_ptr) {
     uint16_t ret_val;
 
-    if(fifo_ptr->read_idx != fifo_ptr->write_idx) {
-        ret_val = fifo_ptr->start_ptr[fifo_ptr->read_idx];
-        fifo_ptr->read_idx = (fifo_ptr->read_idx + 1) % fifo_ptr->N;            // wrap around to end
+    if(fifo_ptr->front_idx != fifo_ptr->back_idx) {                             // ensure FIFO isn't empty
+        ret_val = fifo_ptr->buffer[fifo_ptr->front_idx];
+        fifo_ptr->front_idx = (fifo_ptr->front_idx + 1) % fifo_ptr->N;            // modulo causes wrap around to 0
     }
-    else {                                                                      // FIFO is empty
+    else {
         ret_val = 0;
     }
 
     return ret_val;
 }
 
-void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val) {
-
-    if( (fifo_ptr->write_idx + 1) % 10 != fifo_ptr->read_idx ) {                // ensure FIFO isn't full
-        fifo_ptr->start_ptr[fifo_ptr->write_idx] = val;
-        fifo_ptr->write_idx = (fifo_ptr->write_idx + 1) % fifo_ptr->N;
+void FIFO_Flush(FIFO_t * fifo_ptr, uint16_t output_buffer[]) {
+    uint16_t idx = 0;
+    
+    while((fifo_ptr->front_idx != fifo_ptr->back_idx)) {
+        output_buffer[idx++] = fifo_ptr->buffer[fifo_ptr->front_idx];
+        fifo_ptr->front_idx = (fifo_ptr->front_idx + 1) % fifo_ptr->N;            // wrap around to end
     }
-
 }
 
 /** @} */
--- src/common/FIFO.h
+++ src/common/FIFO.h
@@ -15,30 +15,39 @@
 typedef struct FIFO_t FIFO_t;
 
 /**
- * @brief           Initialize a FIFO buffer of length `N`.
+ * @brief               Initialize a FIFO buffer of length `N`.
  * 
- * @param fifo_ptr  pointer to FIFO object
- * @param buffer    array to be used as FIFO buffer
- * @param N         = (length of buffer) - 1; Should be <= length of buffer
+ * @param fifo_ptr      Pointer to FIFO object
+ * @param buffer        Array to be used as FIFO buffer
+ * @param N             Length of `buffer`. Usable length is `N - 1`.
  * @return FIFO_t* 
  */
 FIFO_t * FIFO_Init(FIFO_t * fifo_ptr, uint16_t buffer[], uint16_t N);
 
 /**
- * @brief           Remove the first value of the buffer.
+ * @brief               Add a value to the end of the buffer.
  * 
- * @param fifo_ptr  pointer to FIFO object
- * @return          uint16_t 
+ * @param fifo_ptr      Pointer to FIFO object
+ * @param val           last value in the buffer
+ */
+void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val);
+
+/**
+ * @brief               Remove the first value of the buffer.
+ * 
+ * @param fifo_ptr      Pointer to FIFO object
+ * @return uint16_t 
  */
 uint16_t FIFO_Get(FIFO_t * fifo_ptr);
 
 /**
- * @brief           Add a value to the end of the buffer.
+ * @brief               Empty the FIFO buffer's contents.
  * 
- * @param fifo_ptr  pointer to FIFO object
- * @param val       last value in the buffer
+ * @param fifo_ptr      Pointer to FIFO object
+ * @param output_buffer Array to output values to.
+ *                      Should be the same length as the FIFO buffer.
  */
-void FIFO_Put(FIFO_t * fifo_ptr, uint16_t val);
+void FIFO_Flush(FIFO_t * fifo_ptr, uint16_t output_buffer[]);
 
 #endif // __FIFO_H__
 
