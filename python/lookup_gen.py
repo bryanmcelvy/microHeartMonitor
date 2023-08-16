f94369d56c536ca20e27fcce2aa11e68ff74f11e
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-16 00:01:06 -0500

    Convert ADC LUT vals to float32_t

--- python/lookup_gen.py
+++ python/lookup_gen.py
@@ -5,16 +5,16 @@ m = 2**11 / 3.3
 b = 2**11
 
 with open("python/adc_lookup.txt", "w") as file:
-    file.write("static const double ADC_LOOKUP[4096] = {\n\t")
+    file.write("static const float32_t ADC_LOOKUP[4096] = {\n" + (4 * " "))
 
     for y in range(4096):
-        x = np.float16((y - b) / m)
-        file.write(f"{x}")
+        x = np.float32((y - b) / m)
 
-        if (y + 1) % 5 == 0:  # multiple of 5
-            file.write(",\n\t")
-        elif y == 4095:  # last number
+        file.write(f"{x}")
+        if y == 4095:  # last number
             file.write("\n};")
+        elif (y + 1) % 4 == 0:
+            file.write(",\n" + (4 * " "))
         else:
             file.write(", ")
 
--- src/common/lookup.h
+++ src/common/lookup.h
@@ -4,9 +4,16 @@
  * @author  Bryan McElvy
  */
 
+#ifndef __LOOKUP_H__
+#define __LOOKUP_H__
+
+#include "arm_math_types.h"
+
 /**
- * @brief                   Return a pointer to the ADC lookup table.
+ * @brief   Return a pointer to the ADC lookup table.
  *
- * @return const double*
+ * @return  const float32_t*
  */
-const double * Lookup_GetPtr_ADC(void);
+const float32_t * Lookup_GetPtr_ADC(void);
+
+#endif
