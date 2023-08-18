0096a3ce2bcc7529235d00d90992bf6fcd9f0986
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-18 18:04:08 -0500

    Start debug module

--- src/app/Debug.c
+++ src/app/Debug.c
@@ -3,3 +3,22 @@
 #include "UART.h"
 
 #include "tm4c123gh6pm.h"
+#include <stdint.h>
+
+char * const MSG_LIST[] = { "Starting transmission...\r\nDebug module initialized.\r\n",
+                            "Data acquisition module initialized.\r\n",
+                            "QRS module initialized.\r\n", "LCD module initialized.\r\n" };
+
+void Debug_Init(void) {
+    UART0_Init();
+    Debug_SendMsg(MSG_LIST[START_MSG]);
+}
+
+void Debug_SendMsg(void * message) {
+    UART0_IRQ_AddStr(message);
+    UART0_IRQ_Start();
+}
+
+void Debug_SendFromList(uint8_t msg_idx) {
+    Debug_SendMsg(MSG_LIST[msg_idx]);
+}
--- src/app/Debug.h
+++ src/app/Debug.h
@@ -11,4 +11,26 @@
 
 #include "tm4c123gh6pm.h"
 
+enum messages { START_MSG, DAQ_INIT, QRS_INIT, LCD_INIT };
+
+/******************************************************************************
+[SECTION NAME]
+*******************************************************************************/
+
+/**
+ * @brief           Initialize the Debug module and send a start message to the
+ *                  serial port.
+ *
+ */
+void Debug_Init(void);
+
+/**
+ * @brief           Send a message to the serial port.
+ *
+ * @param message   (Pointer to) array of ASCII characters.
+ */
+void Debug_SendMsg(void * message);
+
+void Debug_SendFromList(uint8_t msg_idx);
+
 #endif
