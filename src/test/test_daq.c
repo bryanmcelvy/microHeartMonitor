731ea94d807802465564b7cda5fa551be351c1fd
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-19 22:09:12 -0500

    Refactor DAQ test

--- src/test/test_daq.c
+++ src/test/test_daq.c
@@ -4,10 +4,13 @@
  * @brief   Test script for the data acquisition (DAQ) module.
  */
 
-// Includes
-#include "ADC.h"
-#include "DAQ.h"
+/********************************************************************************/
 
+// Includes
+#include "Debug.h"
+#include "LCD.h"
+
+#include "ADC.h"
 #include "GPIO.h"
 #include "PLL.h"
 #include "Timer.h"
@@ -18,20 +21,29 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// Declarations
 #define DAQ_BUFFER_SIZE 16
 
-// Declarations
 volatile FIFO_t * input_fifo_ptr = 0;
 volatile uint32_t input_buffer[DAQ_BUFFER_SIZE] = { 0 };
 volatile bool sampleReady = false;
 
-// Main Function
+/********************************************************************************/
+
 int main(void) {
-    uint16_t x;
-    uint16_t sample;
+    uint16_t x, y;
+    volatile float32_t sample;
 
     PLL_Init();
+    Debug_Init();
 
+    // Initialize/configure LCD
+    LCD_Init();
+    LCD_toggleInversion();
+    LCD_setColor_3bit(LCD_WHITE - LCD_RED);
+    LCD_toggleOutput();
+
+    // Init. ADC
     input_fifo_ptr = FIFO_Init(input_buffer, DAQ_BUFFER_SIZE);
     ADC_Init();
     Timer3A_Init(5);
@@ -40,18 +52,34 @@ int main(void) {
 
     x = 0;
     while(1) {
-        if(sampleReady) {
-            // collect sample
-            ADC_InterruptDisable();
-            sample = (uint16_t) FIFO_Get(input_fifo_ptr);
-            sampleReady = !FIFO_isEmpty(input_fifo_ptr);
-            ADC_InterruptEnable();
+        while(sampleReady == false) {}
+
+        // collect sample
+        ADC_InterruptDisable();
+        sample = ADC_ConvertToVolts(FIFO_Get(input_fifo_ptr));
+        sampleReady = !FIFO_isEmpty(input_fifo_ptr);
+        ADC_InterruptEnable();
+
+        Debug_WriteFloat(sample);
+
+        // plot sample
+        y = (uint16_t) ((sample / LOOKUP_ADC_MAX) * (Y_MAX - 1));
+        LCD_drawRectangle(x, 1, y, 1, true);
+        x = (x + 1) % X_MAX;
+
+        // reset display upon wrapping around to x = 0
+        if(x == 0) {
+            LCD_setColor_3bit(LCD_WHITE - LCD_BLACK);
+            LCD_fill();
+            LCD_setColor_3bit(LCD_WHITE - LCD_RED);
         }
     }
 }
 
+/********************************************************************************/
+
 void ADC0_SS3_Handler(void) {
     FIFO_Put(input_fifo_ptr, (volatile uint32_t)(ADC0_SSFIFO3_R & 0xFFF));
     sampleReady = true;
-    ADC0_ISC_R |= 0x08;                    // clear interrupt flag to acknowledge
+    ADC0_ISC_R |= 0x08;               // clear interrupt flag to acknowledge
 }
