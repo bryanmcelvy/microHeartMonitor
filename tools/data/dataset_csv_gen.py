1a49535481fb0db6a93ce72d9ab12a32ad3fa7f0
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-24 00:17:07 -0500

    Add ECG dataset

---
+++ tools/data/dataset_csv_gen.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from scipy import signal
+import wfdb.io
+
+# fmt: off
+patient_lst = [ 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 
+                110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 
+                120, 121, 122, 123, 124, 200, 201, 202, 203, 204, 
+                205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 
+                215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 
+                225, 226, 227, 228, 229, 230, 231, 232, 233, 234]
+# fmt: on
+
+# Load data
+fs_old = 360
+fs_new = 200
+
+for patient_num in patient_lst:
+    file_name = f"./mit-bih-arr/{patient_num}"
+
+    try:
+        data, _ = wfdb.io.rdsamp(file_name)
+    except:
+        continue
+
+    data = np.transpose(data)[0]
+
+    # Resample from 360 [Hz] to 200 [Hz]
+    N = int((len(data) / fs_old) * fs_new)
+
+    data = pd.DataFrame(signal.resample(data, N))
+    data.to_csv(f"../data/{patient_num}.csv")
