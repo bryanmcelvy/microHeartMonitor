2fe5e4693239557491c671b12951e9b1b5d4f8d0
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-08-27 19:42:08 -0500

    Update ECG test waveform

--- tools/JDS6600/load_ecg_waveform.py
+++ tools/JDS6600/load_ecg_waveform.py
@@ -4,15 +4,15 @@ import pandas as pd
 import time
 
 # Load waveform
-ecg = np.array(pd.read_csv("tools/JDS6600/ecg_waveform.csv"))
+ecg = pd.read_csv("tools/JDS6600/ecg_waveform.csv").iloc[:, 1]
+ecg = ecg.values
 
 # Initialize signal generator
 sig_gen = SigGen("/dev/tty.usbserial-14230")
 sig_gen.turnChannel_OFF(3)
-time.sleep(2)
+time.sleep(1)
 
 # Write waveform to signal generator
-sig_gen.createArbitraryWaveform(1, ecg)
-time.sleep(2)
+sig_gen.createArbitraryWaveform(2, ecg)
 
 print("Done!")
