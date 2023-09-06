e23c570b2347f79b20d7fba471a43bd48487a9c4
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-05 23:22:27 -0500

    Add LED device driver module

---
+++ src/drivers/Led.c
@@ -0,0 +1,96 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Source code for LED module.
+ * @ingroup led
+ */
+
+/// @addtogroup led /// @{
+
+/******************************************************************************
+SECTIONS
+        Preprocessor Directives
+        Initialization
+        Configuration
+        Operations
+*******************************************************************************/
+
+/******************************************************************************
+Preprocessor Directives
+*******************************************************************************/
+
+#include "Led.h"
+
+#include "GPIO_New.h"
+
+#include "NewAssert.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/******************************************************************************
+Initialization
+*******************************************************************************/
+
+struct Led_t {
+    GPIO_Port_t * GPIO_PORT_PTR;               ///< pointer to GPIO port data structure
+    GPIO_Pin_t GPIO_PIN;                       ///< GPIO pin number
+    bool is_ON;                                ///< state indicator
+};
+
+static Led_t Led_ObjPool[LED_POOL_SIZE] = { 0 };
+static uint8_t num_free_leds = LED_POOL_SIZE;
+
+Led_t * Led_Init(GPIO_Port_t * gpioPort_ptr, GPIO_Pin_t pin) {
+    Assert(GPIO_isPortInit(gpioPort_ptr));
+
+    Assert(num_free_leds > 0);
+    num_free_leds -= 1;
+
+    Led_t * led_ptr = &Led_ObjPool[num_free_leds];
+    led_ptr->GPIO_PORT_PTR = gpioPort_ptr;
+    led_ptr->GPIO_PIN = pin;
+    led_ptr->is_ON = false;
+
+    return led_ptr;
+}
+
+/******************************************************************************
+Configuration
+*******************************************************************************/
+
+GPIO_Port_t * Led_GetPort(Led_t * led_ptr) {
+    return led_ptr->GPIO_PORT_PTR;
+}
+
+GPIO_Pin_t LED_GetPin(Led_t * led_ptr) {
+    return led_ptr->GPIO_PIN;
+}
+
+bool Led_isOn(Led_t * led_ptr) {
+    return led_ptr->is_ON;
+}
+
+/******************************************************************************
+Operations
+*******************************************************************************/
+
+void Led_TurnOn(Led_t * led_ptr) {
+    GPIO_WriteHigh(led_ptr->GPIO_PORT_PTR, led_ptr->GPIO_PIN);
+    led_ptr->is_ON = true;
+    return;
+}
+
+void Led_TurnOff(Led_t * led_ptr) {
+    GPIO_WriteLow(led_ptr->GPIO_PORT_PTR, led_ptr->GPIO_PIN);
+    led_ptr->is_ON = false;
+    return;
+}
+
+void Led_Toggle(Led_t * led_ptr) {
+    GPIO_Toggle(led_ptr->GPIO_PORT_PTR, led_ptr->GPIO_PIN);
+    led_ptr->is_ON = !led_ptr->is_ON;
+    return;
+}
+
+/// @}
---
+++ src/drivers/Led.h
@@ -0,0 +1,95 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Interface for LED module.
+ * @ingroup led
+ */
+
+/** @addtogroup led
+ * @{ */
+
+#ifndef __LED_H__
+#define __LED_H__
+
+/******************************************************************************
+Preprocessor Directives
+*******************************************************************************/
+
+#include "GPIO_New.h"
+
+#include "NewAssert.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define LED_POOL_SIZE 3
+
+/******************************************************************************
+Initialization
+*******************************************************************************/
+
+typedef struct Led_t Led_t;
+
+/**
+ * @brief                   Initialize a light-emitting diode (LED) as an `Led_t`.
+ *
+ * @param[in] gpioPort_ptr  Pointer to a `struct` representing a GPIO port.
+ * @param[in] pin           GPIO pin to use.
+ * @param[out] Led_t*       Pointer to LED data structure.
+ */
+Led_t * Led_Init(GPIO_Port_t * gpioPort_ptr, GPIO_Pin_t pin);
+
+/******************************************************************************
+Configuration
+*******************************************************************************/
+
+/**
+ * @brief                       Get the GPIO port associated with the LED.
+ *
+ * @param[in] led_ptr           Pointer to LED data structure.
+ * @param[out] GPIO_Port_t*     Pointer to a GPIO port data structure.
+ */
+GPIO_Port_t * Led_GetPort(Led_t * led_ptr);
+
+/**
+ * @brief                       Get the GPIO pin associated with the LED.
+ *
+ * @param[in] led_ptr           Pointer to LED data structure.
+ * @param[out] GPIO_Pin_t       GPIO pin associated with the LED.
+ */
+GPIO_Pin_t LED_GetPin(Led_t * led_ptr);
+
+/**
+ * @brief                       Check the LED's status.
+ *
+ * @param[in] led_ptr           Pointer to LED data structure.
+ * @param[out] true             the LED is `ON`.
+ * @param[out] false            the LED is `OFF`.
+ */
+bool Led_isOn(Led_t * led_ptr);
+
+/******************************************************************************
+Operations
+*******************************************************************************/
+
+/**
+ * @brief                   Turn the LED `ON`.
+ * @param[in] led_ptr       Pointer to LED data structure.
+ */
+void Led_TurnOn(Led_t * led_ptr);
+
+/**
+ * @brief                   Turn the LED `OFF`.
+ * @param[in] led_ptr       Pointer to LED data structure.
+ */
+void Led_TurnOff(Led_t * led_ptr);
+
+/**
+ * @brief                   Toggle the LED (i.e. `OFF` -> `ON` or `ON` -> `OFF`).
+ * @param[in] led_ptr       Pointer to LED data structure.
+ */
+void Led_Toggle(Led_t * led_ptr);
+
+#endif               // __LED_H__
+
+/// @}
