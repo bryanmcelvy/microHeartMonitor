1db33ba950053f338758f1cbcf667a05c8be20cd
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-07 18:33:38 -0500

    Add test script for new GPIO module

---
+++ src/test/test_gpio_new.c
@@ -0,0 +1,17 @@
+#include "PLL.h"
+#include "GPIO_New.h"
+#include "Led.h"
+#include "Timer.h"
+
+int main(void) {
+    PLL_Init();
+    Timer0A_Init();
+
+    GPIO_Port_t * portF = GPIO_InitPort(F);
+    Led_t * led = Led_Init(portF, GPIO_PIN3);
+
+    while(1) {
+        Led_Toggle(led);
+        Timer0A_Wait1ms(333);
+    }
+}
