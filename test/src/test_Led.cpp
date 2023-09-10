89d1d434ae96bd762798a9381e887272f27d8c0d
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-10 00:22:28 -0500

    Add LED unit test script

---
+++ test/src/test_Led.cpp
@@ -0,0 +1,23 @@
+// clang-format off
+// NOLINTBEGIN
+
+#include "CppUTest/TestHarness.h"
+
+extern "C" {
+#include "Led.h"
+}
+
+TEST_GROUP(LedGroup) { 
+    void setup(){
+
+    } 
+
+    void teardown()
+    {
+
+    } 
+};
+
+TEST(LedGroup, FirstLedTestCase) {
+    
+}
