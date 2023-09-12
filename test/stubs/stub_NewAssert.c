c2c106d0a7b05143dd9d5651f9b1fa89846baacc
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-12 00:10:53 -0500

    Add test stub versions GPIO and NewAssert modules

---
+++ test/stubs/stub_NewAssert.c
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Stub function for custom `Assert()` function.
+ */
+
+// NOLINTBEGIN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "NewAssert.h"
+
+#include <stdbool.h>
+
+void Assert(bool condition) {
+    return;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+// NOLINTEND
