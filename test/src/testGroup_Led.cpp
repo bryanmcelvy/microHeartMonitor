21d4123307a8722f23bce619d03e8a5a222f5fea
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-14 21:08:14 -0500

    Reorganize Led test group

--- test/src/testGroup_Led.cpp
+++ test/src/testGroup_Led.cpp
@@ -8,42 +8,12 @@ extern "C" {
 #include "GPIO.h"
 }
 
-/******************************************************************************
-Initialization
-*******************************************************************************/
-
-TEST_GROUP(Group_LedInit) { 
-    GPIO_Port_t * port;
-
-    void setup() {
-        port = GPIO_InitPort(A);
-        GPIO_ConfigDirOutput(port, GPIO_PIN0);
-        GPIO_EnableDigital(port, GPIO_PIN0);
-    } 
-
-    void teardown()
-    {
-    } 
-};
-
-TEST(Group_LedInit, Test_Init) {
-    Led_t * led = Led_Init(port, GPIO_PIN0);
-    CHECK_FALSE(Led_isOn(led));
-}
-
-/******************************************************************************
-Normal Functions
-*******************************************************************************/
-
 TEST_GROUP(Group_Led) { 
     GPIO_Port_t * port;
     Led_t * led;
 
     void setup() {
         port = GPIO_InitPort(B);
-        GPIO_ConfigDirOutput(port, GPIO_PIN0);
-        GPIO_EnableDigital(port, GPIO_PIN0);
-
         led = Led_Init(port, GPIO_PIN0);
     } 
 
@@ -52,33 +22,45 @@ TEST_GROUP(Group_Led) {
     } 
 };
 
-TEST(Group_Led, Test_GetPortReturnsCorrectPort) {
+/******************************************************************************
+Initialization
+*******************************************************************************/
+
+TEST(Group_Led, isOnAfterInit) {
+    CHECK_FALSE(Led_isOn(led));
+}
+
+/******************************************************************************
+Normal Functions
+*******************************************************************************/
+
+TEST(Group_Led, GetPortReturnsCorrectPort) {
     GPIO_Port_t * newPort = Led_GetPort(led);
     CHECK_EQUAL(port, newPort);
 }
 
-TEST(Group_Led, Test_GetPinReturnsCorrectPin) {
+TEST(Group_Led, GetPinReturnsCorrectPin) {
     GPIO_Pin_t pin = Led_GetPin(led);
     CHECK_EQUAL(GPIO_PIN0, pin);
 }
 
-TEST(Group_Led, Test_isOnAfterTurnOn) {
+TEST(Group_Led, isOnAfterTurnOn) {
     Led_TurnOn(led);
     CHECK_TRUE(Led_isOn(led));
 }
 
-TEST(Group_Led, Test_isOffAfterTurnOff) {
+TEST(Group_Led, isOffAfterTurnOff) {
     Led_TurnOff(led);
     CHECK_FALSE(Led_isOn(led));
 }
 
-TEST(Group_Led, Test_isOnAfterToggleAfterTurnOff) {
+TEST(Group_Led, isOnAfterToggleAfterTurnOff) {
     Led_TurnOff(led);
     Led_Toggle(led);
     CHECK_TRUE(Led_isOn(led));
 }
 
-TEST(Group_Led, Test_isOffAfterToggleAfterTurnOn) {
+TEST(Group_Led, isOffAfterToggleAfterTurnOn) {
     Led_TurnOn(led);
     Led_Toggle(led);
     CHECK_FALSE(Led_isOn(led));
