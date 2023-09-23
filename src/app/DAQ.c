73e1923854102a8a13739ade15755e42b4b2fcde
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-23 00:53:56 -0500

    Use pointer vars in DAQ func calls rather than `&`

--- src/app/DAQ.c
+++ src/app/DAQ.c
@@ -20,6 +20,7 @@ Preprocessor Directives
 
 #include "arm_math_types.h"
 #include "dsp/filtering_functions.h"
+#include "lookup.h"
 #include "tm4c123gh6pm.h"
 
 #include <math.h>
@@ -35,50 +36,47 @@ typedef arm_biquad_casd_df1_inst_f32 filt_t;
 
 enum {
     // Input Filter: 2nd Order Low Pass Filter (fc = 40 [Hz])
-    NUM_STAGES_INPUT = 2,
-    NUM_COEFF_INPUT = NUM_STAGES_INPUT * 5,
-    STATE_BUFF_SIZE_INPUT = NUM_STAGES_INPUT * 4,
+    NUM_STAGES_LOWPASS = 2,
+    NUM_COEFF_INPUT = NUM_STAGES_LOWPASS * 5,
+    STATE_BUFF_SIZE_LOWPASS = NUM_STAGES_LOWPASS * 4,
 
     // Notch Filter: 1st Order 60 [Hz] Bandstop Filter
-    NUM_STAGES_NOTCH = 1,
+    NUM_STAGES_NOTCH = 2,
     NUM_COEFF_NOTCH = NUM_STAGES_NOTCH * 5,
     STATE_BUFF_SIZE_NOTCH = NUM_STAGES_NOTCH * 4
 };
 
-// clang-format off
-
-static const float32_t DAQ_FILT_COEFF_INPUT[NUM_COEFF_INPUT] = { 
-    (float32_t) 0.046582907, (float32_t) 0.093165815, (float32_t) 0.04658291,
-    (float32_t) -0.32897568, (float32_t) 0.06458765,
-    (float32_t) 1.0, (float32_t) 2.0, (float32_t) 0.99999994,
-    (float32_t) -0.45311952, (float32_t) 0.4663256
+static const float32_t FILT_COEFF_LOWPASS[NUM_COEFF_INPUT] = {
+    0.046582907f, 0.093165815f, 0.04658291f, -0.32897568f, 0.06458765f,
+    1.0f,         2.0f,         0.99999994f, -0.45311952f, 0.4663256f
 };
 
 // TODO: Fix notch filter
-static const float32_t DAQ_FILT_COEFF_NOTCH[NUM_COEFF_NOTCH] = {
-    0.7548828125f,
-    0.490478515625f,
-    0.7548828125f,
-    0.490478515625f,
-    0.509765625f
+static const float32_t FILT_COEFF_NOTCH[NUM_COEFF_NOTCH] = {
+    0.9565432255568765f, 0.5914680788951381f, 0.9565432255568764f,
+    0.5631667954285263f, 0.9562366248653332f, 1.0f,
+    0.618339101770126f,  0.9999999999999999f, 0.6460562453305682f,
+    0.9568508578410597f
 };
-// clang-format on
 
-static filt_t inputFilter = { 0 };
-static float32_t stateBuffer_Input[STATE_BUFF_SIZE_INPUT];
+static filt_t lowPassFiltStruct = { 0 };
+static filt_t * const lowPassFilter = &lowPassFiltStruct;
+static float32_t stateBuffer_LowPass[STATE_BUFF_SIZE_LOWPASS];
 
-static filt_t notchFilter = { 0 };
+static filt_t notchFiltStruct = { 0 };
+static filt_t * const notchFilter = &notchFiltStruct;
 static float32_t stateBuffer_Notch[STATE_BUFF_SIZE_NOTCH];
 
 /******************************************************************************
 Functions
 *******************************************************************************/
 void DAQ_Init(void) {
-    arm_biquad_cascade_df1_init_f32((filt_t *) &inputFilter, NUM_STAGES_INPUT, DAQ_FILT_COEFF_INPUT,
-                                    stateBuffer_Input);
-    // arm_biquad_cascade_df1_init_f32((filt_t *) &notchFilter, NUM_STAGES_NOTCH,
-    // DAQ_FILT_COEFF_NOTCH,
-    //                                 stateBuffer_Notch);
+
+    arm_biquad_cascade_df1_init_f32(lowPassFilter, NUM_STAGES_LOWPASS, FILT_COEFF_LOWPASS,
+                                    stateBuffer_LowPass);
+    arm_biquad_cascade_df1_init_f32(notchFilter, NUM_STAGES_NOTCH, FILT_COEFF_NOTCH,
+                                    stateBuffer_Notch);
+
     ADC_Init();
     Timer3A_Init(SAMPLING_PERIOD_MS);
 
@@ -86,17 +84,22 @@ void DAQ_Init(void) {
 }
 
 float32_t DAQ_Filter(volatile float32_t inputSample) {
-    volatile float32_t outputSample1 = 0;
-    volatile float32_t outputSample2 = 0;
+    float32_t * inputPtr = &inputSample;
 
-    arm_biquad_cascade_df1_f32((const filt_t *) &inputFilter, (const float32_t *) &inputSample,
-                               (float32_t *) &outputSample1, 1);
-    // arm_biquad_cascade_df1_f32((const filt_t *) &notchFilter, (const float32_t *) &outputSample1,
-    //                            (float32_t *) &outputSample2, 1);
+    // Low Pass Filter
+    float32_t outputSample = 0;
+    float32_t * outputPtr = &outputSample;
+    arm_biquad_cascade_df1_f32(lowPassFilter, inputPtr, outputPtr, 1);
+    Assert(isinf(outputSample) == false);
 
-    // Assert(isinf(outputSample2) == false);
+    // Notch Filter
+    float32_t tmp = outputSample;
+    float32_t * tmpPtr = &tmp;
+    arm_biquad_cascade_df1_f32(notchFilter, tmpPtr, outputPtr, 1);
+    Assert(isinf(outputSample) == false);
+    // Assert((outputSample > (LOOKUP_ADC_MIN * 4)) && (outputSample < (LOOKUP_ADC_MAX * 4)));
 
-    return outputSample1;
+    return outputSample;
 }
 
 /** @} */
