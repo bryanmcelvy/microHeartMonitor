3eed5cccb387f7cb510749e2d3895ff196df3ed8
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-27 16:18:32 -0500

    Start test script for QRS module

---
+++ src/test/test_qrs.c
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @author  Bryan McElvy
+ * @brief   QRS detector test script.
+ */
+
+#include "PLL.h"
+#include "QRS.h"
+#include "DAQ.h"
+#include "Debug.h"
+
+int main(void) {
+    PLL_Init();
+    while(1) {}
+}
