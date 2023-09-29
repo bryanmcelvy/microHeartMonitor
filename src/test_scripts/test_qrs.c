a7355b79902b0949becf24fa5608ebf03cd7a221
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-28 22:37:49 -0500

    Update QRS test script

--- src/test_scripts/test_qrs.c
+++ src/test_scripts/test_qrs.c
@@ -32,52 +32,59 @@ Declarations
 ******************************************************************************/
 
 enum {
-    DAQ_BUFFER_SIZE = 64,
+    DAQ_BUFFER_CAPACITY = QRS_NUM_SAMP,
+    DAQ_BUFFER_SIZE = DAQ_BUFFER_CAPACITY + 1,
+    QRS_BUFFER_SIZE = QRS_NUM_SAMP + 1
 };
 
 volatile FIFO_t * DAQ_Fifo = 0;
 volatile uint32_t DAQ_Buffer[DAQ_BUFFER_SIZE] = { 0 };
 volatile bool sampleReady = false;
 
+FIFO_t * QRS_Fifo = 0;
+float32_t QRS_FifoBuffer[QRS_BUFFER_SIZE] = { 0 };
+float32_t QRS_InputBuffer[QRS_NUM_SAMP] = { 0 };
+float32_t QRS_OutputBuffer[QRS_NUM_SAMP] = { 0 };
+
 /******************************************************************************
 Main
 ******************************************************************************/
 
 int main(void) {
-    float32_t QRS_FifoBuffer[QRS_NUM_SAMP + 1] = { 0 };
-    float32_t QRS_InputBuffer[QRS_NUM_SAMP] = { 0 };
-    float32_t QRS_OutputBuffer[QRS_NUM_SAMP] = { 0 };
-
-    ISR_GlobalDisable();
-
     PLL_Init();
+    ISR_GlobalDisable();
     Debug_Init();
+
+    QRS_Fifo = FIFO_Init(QRS_FifoBuffer, QRS_BUFFER_SIZE);
+    QRS_Init();
+    Debug_SendFromList(DEBUG_QRS_INIT);
 
     DAQ_Fifo = FIFO_Init(DAQ_Buffer, DAQ_BUFFER_SIZE);
     DAQ_Init();
     Debug_SendFromList(DEBUG_DAQ_INIT);
 
-    FIFO_t * QRS_Fifo = FIFO_Init(QRS_FifoBuffer, QRS_NUM_SAMP + 1);
-    QRS_Init();
-    Debug_SendFromList(DEBUG_QRS_INIT);
-
     ISR_GlobalEnable();
     while(1) {
         while(sampleReady == false) {}
 
-        // collect sample
-        volatile uint16_t raw_sample = FIFO_Get(DAQ_Fifo);
+        while(FIFO_isEmpty(DAQ_Fifo) == false) {
+            // collect sample
+            volatile uint16_t raw_sample = FIFO_Get(DAQ_Fifo);
+
+            // convert and filter
+            volatile float32_t sample = ADC_ConvertToVolts(raw_sample);
+            sample = DAQ_Filter(sample);
+
+            // QRS detection
+            FIFO_Put(QRS_Fifo, sample);
+        }
         sampleReady = !FIFO_isEmpty(DAQ_Fifo);
 
-        // convert and filter
-        volatile float32_t sample = ADC_ConvertToVolts(raw_sample);
-        sample = DAQ_Filter(sample);
-
-        // QRS detection
-        FIFO_Put(QRS_Fifo, sample);
         if(FIFO_isFull(QRS_Fifo)) {
             FIFO_Flush(QRS_Fifo, QRS_InputBuffer);
-            float32_t heartRate_bpm = QRS_RunDetection(QRS_InputBuffer, QRS_OutputBuffer);
+
+            QRS_Preprocess(QRS_InputBuffer, QRS_OutputBuffer);
+            float32_t heartRate_bpm = QRS_ApplyDecisionRules(QRS_OutputBuffer);
             Debug_WriteFloat(heartRate_bpm);
         }
     }
