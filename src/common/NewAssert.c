731cfb1e922b9ce008d9b861a8ec92600125970b
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-29 19:07:28 -0500

    Simplify NewAssert implementation

--- src/common/NewAssert.c
+++ src/common/NewAssert.c
@@ -11,22 +11,17 @@
 
 #include <stdbool.h>
 
-static void Assert_Handler(void);
-
 void Assert(bool condition) {
-    if(condition == false) {
-        Assert_Handler();
+    if(condition) {
+        return;
     }
-
-    return;
-}
-
-static void Assert_Handler(void) {
+    else {
 #ifdef __arm__
-    __asm__("BKPT #0");
+        __asm__("BKPT #0");
 #else
-    while(1) {}
+        while(1) {}
 #endif
+    }
 }
 
 /** @} */
