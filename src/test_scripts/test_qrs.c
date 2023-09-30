95f0ada8c9738ad9e59841f08f292e946c68f700
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-09-29 21:20:21 -0500

    Refactor QRS script to use multiple ISRs

--- src/test_scripts/test_qrs.c
+++ src/test_scripts/test_qrs.c
@@ -24,6 +24,7 @@ Preprocessor Directives
 
 // Other
 #include "arm_math_types.h"
+#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -31,8 +32,11 @@ Preprocessor Directives
 Declarations
 ******************************************************************************/
 
+static void DAQ_Handler(void);
+const uint8_t DAQ_VECTOR_NUM = INT_CAN0;
+
 enum {
-    DAQ_BUFFER_CAPACITY = QRS_NUM_SAMP,
+    DAQ_BUFFER_CAPACITY = 16,
     DAQ_BUFFER_SIZE = DAQ_BUFFER_CAPACITY + 1,
     QRS_BUFFER_SIZE = QRS_NUM_SAMP + 1
 };
@@ -52,8 +56,10 @@ Main
 
 int main(void) {
     PLL_Init();
-    ISR_GlobalDisable();
     Debug_Init();
+
+    ISR_GlobalDisable();
+    ISR_InitNewTableInRam();
 
     QRS_Fifo = FIFO_Init(QRS_FifoBuffer, QRS_BUFFER_SIZE);
     QRS_Init();
@@ -61,44 +67,43 @@ int main(void) {
 
     DAQ_Fifo = FIFO_Init(DAQ_Buffer, DAQ_BUFFER_SIZE);
     DAQ_Init();
+    ISR_addToIntTable(DAQ_Handler, DAQ_VECTOR_NUM);
+    ISR_setPriority(DAQ_VECTOR_NUM, 1);
+    ISR_Enable(DAQ_VECTOR_NUM);
     Debug_SendFromList(DEBUG_DAQ_INIT);
 
     ISR_GlobalEnable();
     while(1) {
-        while(sampleReady == false) {}
-
-        while(FIFO_isEmpty(DAQ_Fifo) == false) {
-            // collect sample
-            volatile uint16_t raw_sample = FIFO_Get(DAQ_Fifo);
-
-            // convert and filter
-            volatile float32_t sample = ADC_ConvertToVolts(raw_sample);
-            sample = DAQ_Filter(sample);
-
-            // QRS detection
-            FIFO_Put(QRS_Fifo, sample);
-        }
-        sampleReady = !FIFO_isEmpty(DAQ_Fifo);
-
         if(FIFO_isFull(QRS_Fifo)) {
             FIFO_Flush(QRS_Fifo, QRS_InputBuffer);
 
+            Debug_SendMsg("Starting QRS detection...");
+
             QRS_Preprocess(QRS_InputBuffer, QRS_OutputBuffer);
             float32_t heartRate_bpm = QRS_ApplyDecisionRules(QRS_OutputBuffer);
+            Debug_Assert(isnan(heartRate_bpm) == false);
+            Debug_Assert(isinf(heartRate_bpm) == false);
             Debug_WriteFloat(heartRate_bpm);
         }
     }
 }
 
 /******************************************************************************
-ADC ISR
+ISRs
 ******************************************************************************/
 
 void ADC0_SS3_Handler(void) {
     Debug_Assert(FIFO_isFull(DAQ_Fifo) == false);
     FIFO_Put(DAQ_Fifo, (volatile uint32_t)(ADC0_SSFIFO3_R & 0xFFF));
-    sampleReady = true;
 
     ADC0_ISC_R |= 0x08;               // clear interrupt flag to acknowledge
-    return;
+    ISR_triggerInterrupt(DAQ_VECTOR_NUM);
+}
+
+void DAQ_Handler(void) {
+    //...
+    volatile uint16_t raw_sample = FIFO_Get(DAQ_Fifo);
+    volatile float32_t sample = ADC_ConvertToVolts(raw_sample);
+    sample = DAQ_Filter(sample);
+    FIFO_Put(QRS_Fifo, sample);
 }
