ed7fcfc1217170a42c0420152ee6e275d2d39c59
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-10-01 16:59:59 -0500

    Rename `float32_t` LUT gen. script

--- tools/lookup_table/lookup_gen_f32.py
+++ tools/lookup_table/lookup_gen_f32.py
@@ -19,7 +19,7 @@ v_in = (v_out - b_1) / m_1                      # [-5.5, 5.5)
 
 
 # output to .txt file
-with open("tools/lookup_table/adc_lookup.txt", "w") as file:
+with open("tools/lookup_table/adc_lookup_f32.txt", "w") as file:
     file.write("static const float32_t ADC_LOOKUP[4096] = {\n" + (4 * " "))
 
     for n in range(NUM_VALS):
