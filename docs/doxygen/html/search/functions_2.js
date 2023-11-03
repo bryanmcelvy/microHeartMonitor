9d680fe8028803cd115e30a171d2674d7c8b0227
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-11-03 17:22:52 -0500

    Disable most group graphs in docs

--- docs/doxygen/html/search/functions_2.js
+++ docs/doxygen/html/search/functions_2.js
@@ -1,16 +1,16 @@
 var searchData=
 [
-  ['fifo_5fflush_0',['FIFO_Flush',['../group__fifo.html#ga7fc114414a6171bab23317d02e7077bf',1,'Fifo.c']]],
-  ['fifo_5fflushfloat_1',['FIFO_FlushFloat',['../group__fifo.html#gaabd652676894094bec0ed79e7892be38',1,'Fifo.c']]],
-  ['fifo_5fget_2',['FIFO_Get',['../group__fifo.html#ga086981b6f2851ab9ac7e9ed6c06046be',1,'Fifo.c']]],
-  ['fifo_5fgetcurrsize_3',['FIFO_getCurrSize',['../group__fifo.html#ga05e8dac96888da8a28db5337d4dd95c9',1,'Fifo.c']]],
-  ['fifo_5fgetfloat_4',['FIFO_GetFloat',['../group__fifo.html#gaa5263a767ddc95fb7975f825f929a924',1,'Fifo.c']]],
-  ['fifo_5finit_5',['FIFO_Init',['../group__fifo.html#ga525c571d68ff9ac5100c83e27283b113',1,'Fifo.c']]],
-  ['fifo_5fisempty_6',['FIFO_isEmpty',['../group__fifo.html#ga89c83b8b9afc2d78f9d03a6a34eef0b6',1,'Fifo.c']]],
-  ['fifo_5fisfull_7',['FIFO_isFull',['../group__fifo.html#gacf3244c97c4f567bc1ee42c926f228de',1,'Fifo.c']]],
-  ['fifo_5fpeekall_8',['FIFO_PeekAll',['../group__fifo.html#gaf5e5f067eee84e89063cf45faf4418e6',1,'Fifo.c']]],
-  ['fifo_5fpeekone_9',['FIFO_PeekOne',['../group__fifo.html#gac0a9a1831d213f0b28b762969b8a8a6b',1,'Fifo.c']]],
-  ['fifo_5fput_10',['FIFO_Put',['../group__fifo.html#ga4fc0e1d9f0ab9af996430200051f022f',1,'Fifo.c']]],
-  ['fifo_5fputfloat_11',['FIFO_PutFloat',['../group__fifo.html#ga8adfbc9bf1730dc691e5d10073ab823d',1,'Fifo.c']]],
-  ['fifo_5freset_12',['FIFO_Reset',['../group__fifo.html#ga2d5c86b5fe502bb97abf6e2ae493f3ce',1,'Fifo.c']]]
+  ['fifo_5fflush_0',['Fifo_Flush',['../group__fifo.html#ga52f0797f2c3af8db878babcdacc6242c',1,'Fifo.c']]],
+  ['fifo_5fflushfloat_1',['Fifo_FlushFloat',['../group__fifo.html#ga6e95564c4286c38fbec21c15213566ad',1,'Fifo.c']]],
+  ['fifo_5fget_2',['Fifo_Get',['../group__fifo.html#ga6a20ad9b707e4b8ccbef29f6ebaf7ea8',1,'Fifo.c']]],
+  ['fifo_5fgetcurrsize_3',['Fifo_getCurrSize',['../group__fifo.html#ga7c7a581b982e97b236cdc2840ce956f0',1,'Fifo.c']]],
+  ['fifo_5fgetfloat_4',['Fifo_GetFloat',['../group__fifo.html#gaa380ef9605518fc998d51d06cc2b65fc',1,'Fifo.c']]],
+  ['fifo_5finit_5',['Fifo_Init',['../group__fifo.html#ga633860451fbd13513862ed1126cea143',1,'Fifo.c']]],
+  ['fifo_5fisempty_6',['Fifo_isEmpty',['../group__fifo.html#gae944d60dc7c317e611e1a06712eb60a3',1,'Fifo.c']]],
+  ['fifo_5fisfull_7',['Fifo_isFull',['../group__fifo.html#ga27bfb943ac834fd7a1bf79cbce3e4af8',1,'Fifo.c']]],
+  ['fifo_5fpeekall_8',['Fifo_PeekAll',['../group__fifo.html#gab70a834e9ca12db9817f15f9342d9fb7',1,'Fifo.c']]],
+  ['fifo_5fpeekone_9',['Fifo_PeekOne',['../group__fifo.html#ga29a8388a2915767fa7885b0f1627d1f0',1,'Fifo.c']]],
+  ['fifo_5fput_10',['Fifo_Put',['../group__fifo.html#gaf68b2cfb40ab050d1de771f7ce276ae1',1,'Fifo.c']]],
+  ['fifo_5fputfloat_11',['Fifo_PutFloat',['../group__fifo.html#ga666d3b6d48902e82cd5c33883a9ccf23',1,'Fifo.c']]],
+  ['fifo_5freset_12',['Fifo_Reset',['../group__fifo.html#ga0a83842220ad1e1684490f0c8c3ca1e9',1,'Fifo.c']]]
 ];
