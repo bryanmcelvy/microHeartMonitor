077565e2546a8d20b5e80563052c689351d470c9
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-11-22 19:28:15 -0600

    Expand/reorganize FreeRTOS config file

--- external/FreeRTOS/FreeRTOSConfig.h
+++ external/FreeRTOS/FreeRTOSConfig.h
@@ -41,60 +41,103 @@
 
 /* Ensure stdint is only used by the compiler, and not the assembler. */
 
-#define configUSE_PREEMPTION           1
-#define configUSE_IDLE_HOOK            0
-#define configUSE_TICK_HOOK            0
-#define configCPU_CLOCK_HZ             (80000000)
-#define configTICK_RATE_HZ             ((TickType_t) 1000)
-#define configMAX_PRIORITIES           (5)
-#define configMINIMAL_STACK_SIZE       ((unsigned short) 130)
-#define configTOTAL_HEAP_SIZE          ((size_t) 0)
-#define configMAX_TASK_NAME_LEN        (10)
-#define configUSE_TRACE_FACILITY       1
-#define configUSE_16_BIT_TICKS         0
-#define configIDLE_SHOULD_YIELD        1
-#define configUSE_MUTEXES              0
-#define configQUEUE_REGISTRY_SIZE      8
-#define configCHECK_FOR_STACK_OVERFLOW 0
-#define configUSE_RECURSIVE_MUTEXES    0
-#define configUSE_MALLOC_FAILED_HOOK   0
-#define configUSE_APPLICATION_TASK_TAG 0
-#define configUSE_COUNTING_SEMAPHORES  0
-#define configGENERATE_RUN_TIME_STATS  0
+#define configUSE_PREEMPTION                      1
+#define configCPU_CLOCK_HZ                        (80000000)
+#define configSYSTICK_CLOCK_HZ                    configCPU_CLOCK_HZ
+#define configTICK_RATE_HZ                        ((TickType_t) 1000)
+#define configMAX_PRIORITIES                      (8)
+#define configMINIMAL_STACK_SIZE                  (130)
+#define configMAX_TASK_NAME_LEN                   (16)
+#define configUSE_16_BIT_TICKS                    0
+#define configIDLE_SHOULD_YIELD                   1
+#define configQUEUE_REGISTRY_SIZE                 0
+#define configUSE_APPLICATION_TASK_TAG            0
+#define configUSE_COUNTING_SEMAPHORES             0
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION   0
+#define configUSE_TICKLESS_IDLE                   0
+#define configTICK_TYPE_WIDTH_IN_BITS             TICK_TYPE_WIDTH_16_BITS
+#define configUSE_TASK_NOTIFICATIONS              1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES     3
+#define configUSE_MUTEXES                         0
+#define configUSE_RECURSIVE_MUTEXES               0
+#define configUSE_ALTERNATIVE_API                 0 /* Deprecated! */
+#define configUSE_QUEUE_SETS                      0
+#define configUSE_TIME_SLICING                    0
+#define configUSE_NEWLIB_REENTRANT                0
+#define configENABLE_BACKWARD_COMPATIBILITY       0
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS   5
+#define configUSE_MINI_LIST_ITEM                  1
+#define configSTACK_DEPTH_TYPE                    uint16_t
+#define configMESSAGE_BUFFER_LENGTH_TYPE          size_t
+#define configHEAP_CLEAR_MEMORY_ON_FREE           1
+
+/* Memory allocation related definitions. */
+#define configSUPPORT_STATIC_ALLOCATION           1
+#define configSUPPORT_DYNAMIC_ALLOCATION          0
+#define configTOTAL_HEAP_SIZE                     ((size_t) 0)
+#define configAPPLICATION_ALLOCATED_HEAP          0
+#define configSTACK_ALLOCATION_FROM_SEPARATE_HEAP 0
+
+/* Hook function related definitions. */
+#define configUSE_IDLE_HOOK                       0
+#define configUSE_TICK_HOOK                       1
+#define configCHECK_FOR_STACK_OVERFLOW            0
+#define configUSE_MALLOC_FAILED_HOOK              0
+#define configUSE_DAEMON_TASK_STARTUP_HOOK        0
+#define configUSE_SB_COMPLETED_CALLBACK           0
+
+/* Run time and task stats gathering related definitions. */
+#define configGENERATE_RUN_TIME_STATS             0
+#define configUSE_TRACE_FACILITY                  1
+#define configUSE_STATS_FORMATTING_FUNCTIONS      0
+
+/* Co-routine related definitions. */
+#define configUSE_CO_ROUTINES                     0
+#define configMAX_CO_ROUTINE_PRIORITIES           1
 
 /* Software timer definitions. */
-#define configUSE_TIMERS               0
-#define configTIMER_TASK_PRIORITY      (2)
-#define configTIMER_QUEUE_LENGTH       10
-#define configTIMER_TASK_STACK_DEPTH   (configMINIMAL_STACK_SIZE * 2)
+#define configUSE_TIMERS                          0
+#define configTIMER_TASK_PRIORITY                 (2)
+#define configTIMER_QUEUE_LENGTH                  10
+#define configTIMER_TASK_STACK_DEPTH              (configMINIMAL_STACK_SIZE * 2)
 
-/* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet       1
-#define INCLUDE_uxTaskPriorityGet      1
-#define INCLUDE_vTaskDelete            0
-#define INCLUDE_vTaskCleanUpResources  1
-#define INCLUDE_vTaskSuspend           1
-#define INCLUDE_vTaskDelayUntil        1
-#define INCLUDE_vTaskDelay             1
+/* Optional functions - most linkers will remove unused functions anyway. */
+#define INCLUDE_vTaskPrioritySet                  1
+#define INCLUDE_uxTaskPriorityGet                 0
+#define INCLUDE_vTaskDelete                       0
+#define INCLUDE_vTaskSuspend                      1
+#define INCLUDE_xResumeFromISR                    1
+#define INCLUDE_vTaskDelayUntil                   0
+#define INCLUDE_vTaskDelay                        1
+#define INCLUDE_xTaskGetSchedulerState            0
+#define INCLUDE_xTaskGetCurrentTaskHandle         0
+#define INCLUDE_uxTaskGetStackHighWaterMark       0
+#define INCLUDE_uxTaskGetStackHighWaterMark2      0
+#define INCLUDE_xTaskGetIdleTaskHandle            0
+#define INCLUDE_eTaskGetState                     0
+#define INCLUDE_xEventGroupSetBitFromISR          0
+#define INCLUDE_xTimerPendFunctionCall            0
+#define INCLUDE_xTaskAbortDelay                   0
+#define INCLUDE_xTaskGetHandle                    0
+#define INCLUDE_xTaskResumeFromISR                1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
 /* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
 #define configPRIO_BITS __NVIC_PRIO_BITS
 #else
-#define configPRIO_BITS 3 /* 7 priority levels */
+#define configPRIO_BITS 3 /* 8 priority levels */
 #endif
 
 /* The lowest interrupt priority that can be used in a call to a "set priority"
 function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY      0xf
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY      (7)
 
 /* The highest interrupt priority that can be used by any interrupt service
 routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
 PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 1
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
@@ -110,6 +153,7 @@ header file. */
 #define configASSERT(x)                                                                            \
     if((x) == 0) {                                                                                 \
         taskDISABLE_INTERRUPTS();                                                                  \
+        __asm__("BKPT #0");                                                                        \
         for(;;)                                                                                    \
             ;                                                                                      \
     }
@@ -119,5 +163,27 @@ standard names. */
 #define vPortSVCHandler     SVC_Handler
 #define xPortPendSVHandler  PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
+
+//    // Interrupt nesting behaviour configuration.
+//    #define configKERNEL_INTERRUPT_PRIORITY [dependent of processor]
+//    #define configMAX_SYSCALL_INTERRUPT_PRIORITY                                   \
+    //      [dependent on processor and application]
+//    #define configMAX_API_CALL_INTERRUPT_PRIORITY                                  \
+    //      [dependent on processor and application]
+
+//    // FreeRTOS MPU specific definitions.
+//    #define configINCLUDE_APPLICATION_DEFINED_PRIVILEGED_FUNCTIONS 0
+//    #define configTOTAL_MPU_REGIONS 8    /* Default value. */
+//    #define configTEX_S_C_B_FLASH 0x07UL /* Default value. */
+//    #define configTEX_S_C_B_SRAM 0x07UL  /* Default value. */
+//    #define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY 1
+//    #define configALLOW_UNPRIVILEGED_CRITICAL_SECTIONS 1
+//    #define configENABLE_ERRATA_837070_WORKAROUND 1
+//    #define configUSE_MPU_WRAPPERS_V1 0
+//    #define configPROTECTED_KERNEL_OBJECT_POOL_SIZE 10
+//    #define configSYSTEM_CALL_STACK_SIZE 128
+
+//    // ARMv8-M secure side port related definitions.
+//    #define secureconfigMAX_SECURE_CONTEXTS 5
 
 #endif /* FREERTOS_CONFIG_H */
