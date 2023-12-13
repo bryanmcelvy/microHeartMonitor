425a1eb1c8da9ba19c1e2af45cae54683d26e31f
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-12-13 14:12:15 -0600

    Add comments to PWM pin `enum`

--- src/drivers/pwm.h
+++ src/drivers/pwm.h
@@ -6,22 +6,22 @@
 typedef struct PwmStruct_t * Pwm_t;
 
 typedef enum {
-    M0PWM0,
-    M0PWM1,
-    M0PWM2,
-    M0PWM3,
-    M0PWM4,
-    M0PWM5,
-    M0PWM6,
-    M0PWM7,
-    M1PWM0,
-    M1PWM1,
-    M1PWM2,
-    M1PWM3,
-    M1PWM4,
-    M1PWM5,
-    M1PWM6,
-    M1PWM7
+    M0PWM0,               // PB6
+    M0PWM1,               // PB7
+    M0PWM2,               // PB4
+    M0PWM3,               // PB5
+    M0PWM4,               // PE4
+    M0PWM5,               // PE5
+    M0PWM6,               // PC4/PD0
+    M0PWM7,               // PC5/PD1
+    M1PWM0,               // PD0
+    M1PWM1,               // PD1
+    M1PWM2,               // PA6/PE4
+    M1PWM3,               // PA7/PE5
+    M1PWM4,               // PF0
+    M1PWM5,               // PF1
+    M1PWM6,               // PF2
+    M1PWM7,               // PF3
 } PwmPin_e;
 
 Pwm_t Pwm_Init(GpioPort_t gpioPort, PwmPin_e pwmPin);
@@ -38,4 +38,4 @@ void Pwm_Enable(Pwm_t pwm);
 
 void Pwm_Disable(Pwm_t pwm);
 
-#endif               // PWM_H
+#endif                    // PWM_H
