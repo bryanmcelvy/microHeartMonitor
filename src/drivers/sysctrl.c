d56f10294b8234c34c51d07e53392541476425ff
BryanMcElvy <45156849+bryanmcelvy@users.noreply.github.com>
2023-12-15 15:35:06 -0600

    Complete SysCtrl module

--- src/drivers/sysctrl.c
+++ src/drivers/sysctrl.c
@@ -1,3 +1,12 @@
+/**
+ * @addtogroup sysctrl
+ * @{
+ *
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Header file for System Control module.
+ */
+
 #include "sysctrl.h"
 
 #include "NewAssert.h"
@@ -7,6 +16,65 @@
 
 #include <stdbool.h>
 #include <stdint-gcc.h>
+
+/******************************************************************************
+Phase-Locked Loop (PLL)
+*******************************************************************************/
+
+static uint8_t PLL_Divisor;
+
+void SysCtrl_PLL_Init(uint8_t divisor) {
+    assert(divisor > 4);
+    assert(divisor != 7);
+    assert(divisor <= 128);
+
+    // Disable PLL and system clock divider
+    SYSCTL_RCC_R &= ~(SYSCTL_RCC_USESYSDIV);               // disable system clock divider
+    SYSCTL_RCC2_R |= SYSCTL_RCC2_USERCC2;               // use RCC2 register for more freq. options
+    SYSCTL_RCC2_R |= SYSCTL_RCC2_BYPASS2;               // set BYPASS2 to disable PLL
+
+    // Select crystal value and oscillator source
+    SYSCTL_RCC_R = (SYSCTL_RCC_R & ~(SYSCTL_RCC_XTAL_M))               // 16[MHz] crystal
+                   | SYSCTL_RCC_XTAL_16MHZ;
+    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_OSCSRC2_M);                         // use main oscillator
+    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_PWRDN2);                            // power on PLL
+
+    // Set system clock divider
+    SYSCTL_RCC2_R |= SYSCTL_RCC2_DIV400;                                 // enable 7-bit divisor
+    SYSCTL_RCC2_R &=
+        ~(SYSCTL_RCC2_SYSDIV2_M | SYSCTL_RCC2_SYSDIV2LSB);               // clear divisor bits
+    SYSCTL_RCC2_R |= ((divisor - 1) << 22);                              // = (f_PLL / f_bus) - 1
+    SYSCTL_RCC_R |= SYSCTL_RCC_USESYSDIV;               // enable system clock divider
+
+    // Re-activate PLL
+    while((SYSCTL_RIS_R & SYSCTL_RIS_PLLLRIS) == 0) {               // wait for PLL to lock
+        __NOP();
+    }
+    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_BYPASS2);                        // clear BYPASS2 to enable PLL
+
+    PLL_Divisor = divisor;
+
+    return;
+}
+
+uint32_t SysCtrl_getClkFreq_Hz(void) {
+    static uint32_t freq = 16000000;
+
+    // Calculate and return PLL frequency if it's initialized
+    if(SYSCTL_PLLSTAT_R) {
+        uint32_t MINT = SYSCTL_PLLFREQ0_R & SYSCTL_PLLFREQ0_MINT_M;
+        uint32_t MFRAC = (SYSCTL_PLLFREQ0_R & SYSCTL_PLLFREQ0_MFRAC_M) >> 10;
+        uint32_t MDIV = MINT + (MFRAC / 1024);
+
+        uint32_t Q = (SYSCTL_PLLFREQ1_R & SYSCTL_PLLFREQ1_Q_M) >> 8;
+        uint32_t N = SYSCTL_PLLFREQ1_R & SYSCTL_PLLFREQ1_N_M;
+
+        freq = (16 * MDIV) / ((Q + 1) * (N + 1));               // PLL frequency
+        freq /= PLL_Divisor;                                    // clock frequency
+    }
+
+    return freq;
+}
 
 /******************************************************************************
 Clock Gating Control
@@ -32,7 +100,8 @@ enum PERIPHERAL_REGISTER_OFFSETS {
     PWM_OFFSET = (uint32_t) 0x40,
 };
 
-void SysCtrl_setClkGatingCtrl(SysCtrlClkMode_e mode, SysCtrlPeripheral_e peripheral, uint8_t num) {
+void SysCtrl_configPeripheralClk(SysCtrlClkMode_e mode, SysCtrlPeripheral_e peripheral, uint8_t num,
+                                 SysCtrlClkStatus_e status) {
     // Determine register offsets for the mode and peripheral
     uint32_t modeOffset;
     switch(mode) {
@@ -87,10 +156,23 @@ void SysCtrl_setClkGatingCtrl(SysCtrlClkMode_e mode, SysCtrlPeripheral_e periphe
             assert(false);
     }
 
-    // Start clock to peripheral and wait for it to be ready
-    REGISTER_VAL(SYSCTRL_BASE + modeOffset + peripheralOffset) |= (1 << num);
-    while(((REGISTER_VAL(SYSCTRL_BASE + READY_OFFSET + peripheralOffset)) & (1 << num)) == 0) {
-        __NOP();
+    switch(status) {
+        case SYSCTRL_CLK_ON:               // clang-format off
+            // Start clock to peripheral and wait for it to be ready
+            REGISTER_VAL(SYSCTRL_BASE + modeOffset + peripheralOffset) |= (1 << num);
+            while(((REGISTER_VAL(SYSCTRL_BASE + READY_OFFSET + peripheralOffset)) & (1 << num)) == 0) {
+                __NOP();
+            }
+            break;               // clang-format on
+        case SYSCTRL_CLK_OFF:
+            // Stop clock to peripheral and allow it to turn off
+            REGISTER_VAL(SYSCTRL_BASE + modeOffset + peripheralOffset) &= ~(1 << num);
+            while((REGISTER_VAL(SYSCTRL_BASE + READY_OFFSET + peripheralOffset)) & (1 << num)) {
+                __NOP();
+            }
+            break;
+        default:
+            assert(false);
     }
 
     return;
@@ -100,20 +182,43 @@ void SysCtrl_setClkGatingCtrl(SysCtrlClkMode_e mode, SysCtrlPeripheral_e periphe
 Sleep Mode
 *******************************************************************************/
 
-#define CM4_SYSCTRL_ADDR ((uint32_t) 0xE000ED10)
+#define CM4_SYSCTRL_R         (REGISTER_VAL(0xE000ED10))
+#define CM4_SYSCTRL_SEVONPEND ((uint32_t) 0x10)
+#define CM4_SYSCTRL_SLEEPDEEP ((uint32_t) 0x04)
+#define CM4_SYSCTRL_SLEEPXIT  ((uint32_t) 0x02)
+
+void SysCtrl_configSleepMode(SysCtrlSleepConfig_e param) {
+    switch(param) {
+        case SYSCTRL_CFG_WAKE_ON_ENABLED:
+            CM4_SYSCTRL_R &= ~(CM4_SYSCTRL_SEVONPEND);
+            break;
+        case SYSCTRL_CFG_WAKE_ON_PEND:
+            CM4_SYSCTRL_R |= CM4_SYSCTRL_SEVONPEND;
+            break;
+        case SYSCTRL_CFG_NORMAL_SLEEP:
+            CM4_SYSCTRL_R &= ~(CM4_SYSCTRL_SLEEPDEEP);
+            break;
+        case SYSCTRL_CFG_DEEP_SLEEP:
+            CM4_SYSCTRL_R |= CM4_SYSCTRL_SLEEPDEEP;
+            break;
+        case SYSCTRL_CFG_NO_SLEEP_ON_EXIT:
+            CM4_SYSCTRL_R &= ~(CM4_SYSCTRL_SLEEPXIT);
+            break;
+        case SYSCTRL_CFG_SLEEP_ON_EXIT:
+            CM4_SYSCTRL_R |= CM4_SYSCTRL_SLEEPXIT;
+            break;
+        default:
+            assert(false);
+    }
+}
 
 void SysCtrl_enterSleepMode(SysCtrlSleepModes_e sleepMode) {
-    REGISTER_VAL(CM4_SYSCTRL_ADDR) &= ~(0x12);
     switch(sleepMode) {
         case WAIT_FOR_INT:
             __WFI();
             break;
         case WAIT_FOR_EVENT:
-            REGISTER_VAL(CM4_SYSCTRL_ADDR) |= 0x10;
             __WFE();
-            break;
-        case SLEEP_ON_EXIT:
-            REGISTER_VAL(CM4_SYSCTRL_ADDR) |= 0x02;
             break;
         default:
             assert(false);
@@ -121,52 +226,6 @@ void SysCtrl_enterSleepMode(SysCtrlSleepModes_e sleepMode) {
     return;
 }
 
-/******************************************************************************
-Phase-Locked Loop (PLL)
-*******************************************************************************/
+/******************************************************************************/
 
-void SysCtrl_PLL_Init(uint8_t divisor) {
-    assert(divisor > 4);
-    assert(divisor != 7);
-    assert(divisor <= 128);
-
-    // Disable PLL and system clock divider
-    SYSCTL_RCC_R &= ~(SYSCTL_RCC_USESYSDIV);               // disable system clock divider
-    SYSCTL_RCC2_R |= SYSCTL_RCC2_USERCC2;               // use RCC2 register for more freq. options
-    SYSCTL_RCC2_R |= SYSCTL_RCC2_BYPASS2;               // set BYPASS2 to disable PLL
-
-    // Select crystal value and oscillator source
-    SYSCTL_RCC_R = (SYSCTL_RCC_R & ~(SYSCTL_RCC_XTAL_M))               // 16[MHz] crystal
-                   | SYSCTL_RCC_XTAL_16MHZ;
-    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_OSCSRC2_M);                         // use main oscillator
-    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_PWRDN2);                            // power on PLL
-
-    // Set system clock divider
-    SYSCTL_RCC2_R |= SYSCTL_RCC2_DIV400;                     // enable 7-bit divisor
-    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_SYSDIV2_M);               // clear divisor bits
-    SYSCTL_RCC2_R |= ((divisor - 1) << 22);                  // = (f_PLL / f_bus) - 1
-    SYSCTL_RCC_R |= SYSCTL_RCC_USESYSDIV;                    // enable system clock divider
-
-    // Re-activate PLL
-    while((SYSCTL_RIS_R & SYSCTL_RIS_PLLLRIS) == 0) {               // wait for PLL to lock
-        __NOP();
-    }
-    SYSCTL_RCC2_R &= ~(SYSCTL_RCC2_BYPASS2);                        // clear BYPASS2 to enable PLL
-
-    return;
-}
-
-uint32_t SysCtrl_PLL_getFreq(void) {
-    uint32_t MINT = SYSCTL_PLLFREQ0_R & SYSCTL_PLLFREQ0_MINT_M;
-    uint32_t MFRAC = (SYSCTL_PLLFREQ0_R & SYSCTL_PLLFREQ0_MFRAC_M) >> 10;
-    uint32_t MDIV = MINT + (MFRAC / 1024);
-
-    uint32_t Q = (SYSCTL_PLLFREQ1_R & SYSCTL_PLLFREQ1_Q_M) >> 8;
-    uint32_t N = SYSCTL_PLLFREQ1_R & SYSCTL_PLLFREQ1_N_M;
-
-    return (16 * MDIV) / ((Q + 1) * (N + 1));
-}
-
-bool SysCtrl_PLL_isOn(void) {
-    return (bool) SYSCTL_PLLSTAT_R;
-}
+/** @} */               // sysctrl
--- src/drivers/sysctrl.h
+++ src/drivers/sysctrl.h
@@ -1,3 +1,12 @@
+/**
+ * @addtogroup sysctrl
+ * @{
+ *
+ * @file
+ * @author  Bryan McElvy
+ * @brief   Header file for System Control module.
+ */
+
 #ifndef SYSCTRL_H
 #define SYSCTRL_H
 
@@ -5,48 +14,126 @@
 #include <stdint-gcc.h>
 
 /******************************************************************************
+Phase-Locked Loop (PLL)
+*******************************************************************************/
+
+enum PLL_DIVISOR_FREQUENCIES {
+    SYSCTRL_PLL_80MHZ = 5,
+    SYSCTRL_PLL_66_7MHZ = 6,
+    SYSCTRL_PLL_50MHZ = 8,
+    SYSCTRL_PLL_40MHZ = 10,
+    SYSCTRL_PLL_20MHZ = 20,
+    SYSCTRL_PLL_10MHZ = 40,
+    SYSCTRL_PLL_8MHZ = 50,
+    SYSCTRL_PLL_5MHZ = 80,
+    SYSCTRL_PLL_4MHZ = 100,
+};
+
+/**
+ * @brief                       Initialize the phase-locked loop (PLL) to increase
+ *                              the clock frequency.
+ *
+ * @param[in] divisor           7-bit diviser to use. Should be 5, 6, or in range [8, 128].
+ *
+ * @post                        The phase-locked loop is initialized, increasing the system clock
+ *                              frequency to be equal to (400[MHz] / divisor).
+ *
+ * @see                         SysCtrl_getClkFreq_Hz()
+ */
+void SysCtrl_PLL_Init(uint8_t divisor);
+
+/**
+ * @brief                       Return the current clock frequency in `[Hz]`.
+ *
+ * @param[out] freq             Current clock frequency.
+ *                              Value is 16[MHz] if the PLL isn't being used.
+ *
+ * @see                         SysCtrl_PLL_Init()
+ */
+uint32_t SysCtrl_getClkFreq_Hz(void);
+
+/******************************************************************************
 Clock Gating Control
 *******************************************************************************/
 
 typedef enum {
-    ADC,
-    DMA,
-    GPIO,
-    I2C,
-    PWM,
-    SSI,
-    TIMER,
-    UART,
+    RUN,                               ///< run mode
+    SLEEP,                             ///< sleep mode
+    DEEP_SLEEP                         ///< deep sleep mode
+} SysCtrlClkMode_e;
+
+typedef enum {
+    ADC,                               ///< Analog-to-Digital Converter
+    DMA,                               ///< Direct Memory Access
+    GPIO,                              ///< General-Purpose Input/Output
+    I2C,                               ///< Inter-Integrated Circuit
+    PWM,                               ///< Pulse Width Modulator
+    SSI,                               ///< Synchronous Serial Interface
+    TIMER,                             ///< General-Purpose Timer
+    UART,                              ///< Universal Asynchronous Receiver/Transmitter
 } SysCtrlPeripheral_e;
 
 typedef enum {
-    RUN,
-    SLEEP,
-    DEEP_SLEEP
-} SysCtrlClkMode_e;
+    SYSCTRL_CLK_OFF = 0,               ///< turn clock `OFF`
+    SYSCTRL_CLK_ON = 1                 ///< turn clock `ON`
+} SysCtrlClkStatus_e;
 
-void SysCtrl_setClkGatingCtrl(SysCtrlClkMode_e mode, SysCtrlPeripheral_e peripheral, uint8_t num);
+/**
+ * @brief                       Configure the clock for a given peripheral in run mode,
+ *                              sleep mode, or deep-sleep mode.
+ *
+ * @param[in] mode              Mode to configure peripheral for. (See @ref SysCtrlClkMode_e)
+ * @param[in] peripheral        Peripheral to configure clock for. (See @ref SysCtrlPeripheral_e)
+ * @param[in] num               The number for the peripheral.
+ * @param[in] status            The desired status for the peripheral.
+ *                              (See @ref SysCtrlClkStatus_e)
+ *
+ * @post                        If status == `ON`, the peripheral is turned on and ready to access.
+ * @post                        If status == `OFF`, the peripheral is turned off.
+ */
+void SysCtrl_configPeripheralClk(SysCtrlClkMode_e mode, SysCtrlPeripheral_e peripheral, uint8_t num,
+                                 SysCtrlClkStatus_e status);
 
 /******************************************************************************
 Sleep Mode
 *******************************************************************************/
 
 typedef enum {
-    WAIT_FOR_INT,
-    WAIT_FOR_EVENT,
-    SLEEP_ON_EXIT
+    SYSCTRL_CFG_WAKE_ON_ENABLED,                ///< only enabled interrupts trigger wake up event
+    SYSCTRL_CFG_WAKE_ON_PEND,                   ///< all pending interrupts trigger wake up event
+    SYSCTRL_CFG_NORMAL_SLEEP,                   ///< use sleep mode for low power mode
+    SYSCTRL_CFG_DEEP_SLEEP,                     ///< use deep sleep mode for low power mode
+    SYSCTRL_CFG_NO_SLEEP_ON_EXIT,               ///< do not sleep when all int. handlers are done
+    SYSCTRL_CFG_SLEEP_ON_EXIT,                  ///< sleep once all interrupt handlers are done
+} SysCtrlSleepConfig_e;
+
+/**
+ * @brief                       Configure sleep mode.
+ *
+ * @param[in] param             Parameter to set.
+ * @post                        See @ref SysCtrlSleepConfig_e.
+ *
+ * @see                         SysCtrl_enterSleepMode()
+ */
+void SysCtrl_configSleepMode(SysCtrlSleepConfig_e param);
+
+typedef enum {
+    WAIT_FOR_INT,                 ///< sleep until an interrupt occurs
+    WAIT_FOR_EVENT,               ///< sleep until an event occurs
 } SysCtrlSleepModes_e;
 
+/**
+ * @brief                       Enter sleep mode.
+ *
+ * @param[in] param             Parameter to set.
+ * @post                        See @ref SysCtrlSleepModes_e.
+ *
+ * @see                         SysCtrl_configSleepMode()
+ */
 void SysCtrl_enterSleepMode(SysCtrlSleepModes_e sleepMode);
 
-/******************************************************************************
-Phase-Locked Loop (PLL)
-*******************************************************************************/
-
-void SysCtrl_PLL_Init(uint8_t divisor);
-
-uint32_t SysCtrl_PLL_getFreq(void);
-
-bool SysCtrl_PLL_isOn(void);
+/******************************************************************************/
 
 #endif
+
+/** @} */               // sysctrl
